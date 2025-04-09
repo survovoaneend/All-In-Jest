73480e5a981eaccda668f5efaf60550db30b6499
Aiden Anderson <aidenanderson08@outlook.com>
2025-04-08 22:30:03 -0500

    fixed tipteerer bug

--- jokers/tipteerer.lua
+++ jokers/tipteerer.lua
@@ -25,6 +25,7 @@ SMODS.Joker {
 
   loc_vars = function(self, info_queue, card)
     local sell_cost = 0
+    if G.jokers and G.jokers.cards then
       for i = 1, #G.jokers.cards do
         if G.jokers.cards[i] ~= card and (G.jokers.cards[i].area and G.jokers.cards[i].area == G.jokers) then
             sell_cost = sell_cost + G.jokers.cards[i].sell_cost
@@ -36,6 +37,7 @@ SMODS.Joker {
         card.ability.extra.money
       }
     }
+  end
   end,
 
   calculate = function(self, card, context)
