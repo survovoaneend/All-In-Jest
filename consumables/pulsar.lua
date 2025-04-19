ff60fb436f100c559f8c757038093b7bef8c6fc2
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-04-19 23:41:15 +1000

    Used util functions for new spectrals

--- consumables/pulsar.lua
+++ consumables/pulsar.lua
@@ -24,8 +24,6 @@ SMODS.Consumable {
         return true
     end,
 	use = function(self, card, area, copier)
-
-
         -- Display initial UI update for "All Hands"
         update_hand_text({sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3}, {
             handname = localize('k_all_hands'),
@@ -44,14 +42,12 @@ SMODS.Consumable {
             if card then card:juice_up(0.8, 0.5) end
             G.TAROT_INTERRUPT_PULSE = nil
             return true end }))
+        -- Explicitly show no chip change
         update_hand_text({sound = 'button', volume = 0.7, pitch = 0.9, delay = 0}, {level='+1'})
 
         -- Apply the actual stat changes silently
         for hand_key, hand_data in pairs(G.GAME.hands) do
-                local mult_increase = hand_data.l_mult * 2
-                G.GAME.hands[hand_key].mult = G.GAME.hands[hand_key].mult + mult_increase
-
-                G.GAME.hands[hand_key].level = math.max(0, G.GAME.hands[hand_key].level + 1)
+            level_up_hand_mult(card, hand_key, true)
         end
         delay(1.6)
         update_hand_text({sound = 'button', volume = 0.7, pitch = 1.1, delay = 0}, {mult = 0, chips = 0, handname = '', level = ''})
