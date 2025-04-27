88e55c00e13e7e343bd411826b9c952e9c2dbd26
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-04-27 15:29:48 -0400

    Jokers pt.4

---
+++ Items/Jokers/xinmo.lua
@@ -0,0 +1,30 @@
+local xinmo = {
+    object_type = "Joker",
+    order = 297,
+    ignore = true,
+
+    key = "xinmo",
+    config = {
+      
+    },
+    rarity = 4,
+		unlock_condition = {hidden = true},
+    pos = { x = 1, y = 10},
+    atlas = 'legendary_atlas',
+    cost = 4,
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = false,
+    eternal_compat = false,
+    soul_pos = { x = 1, y = 11},
+  
+    loc_vars = function(self, info_queue, card)
+  
+    end,
+  
+    calculate = function(self, card, context)
+      
+    end
+  
+}
+return { name = {"Jokers"}, items = {xinmo} }
