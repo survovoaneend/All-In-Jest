b2e1fbd328720198eeeb33636bc56ff506e2b42b
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-04-30 23:32:25 -0400

    Legend fix

--- Items/Jokers/jesters_privelege.lua
+++ Items/Jokers/jesters_privelege.lua
@@ -19,39 +19,13 @@ local jesters_privelege = {
   
     end,
   
-    calculate = function(self, card, context)
-      if context.reroll_shop and not context.blueprint then
-        local rare_jokers = {}
-        for _, v in ipairs(G.shop_jokers.cards) do
-            if v.config.center.rarity == 3 then
-                table.insert(rare_jokers, v)
-            end
-        end
-        for _, j in ipairs(rare_jokers) do
-            if pseudorandom('jesters_privelege') < 0.2 then
-                j:juice_up(0.3, 0.4)
-                local c = G.shop_jokers:remove_card(j)
-                c:remove()
-                c = nil
+    add_to_deck = function(self, card, from_debuff)
+      G.GAME.jest_legendary_pool.in_shop = true
+      G.GAME.jest_legendary_pool.rate = 0.99 -- 1%
+    end,
 
-                G.E_MANAGER:add_event(Event({
-                    trigger = 'before',
-                    blockable = false,
-                    blocking = true,
-                    func = function()
-                       
-                        local joker = create_card("Joker", G.shop_jokers, true, 4, nil, nil, nil, nil)
-                        create_shop_card_ui(joker)
-                        joker:set_cost()
-                        G.shop_jokers:emplace(joker)
-                        play_sound('holo1', 1.2 + math.random()*0.1, 0.4)
-                        return true
-                    end
-                }))
-                card_eval_status_text(card, 'extra', nil, nil, nil, {message = "Legendary"})
-            end
-        end
-    end
-end
+    remove_from_deck = function(self, card, from_debuff)
+        G.GAME.jest_legendary_pool.in_shop = false
+    end,
 }
 return { name = {"Jokers"}, items = {jesters_privelege} }
