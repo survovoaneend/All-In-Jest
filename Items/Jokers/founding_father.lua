63f50dd482bbab7a2dd85eeab14d8d6146d9c710
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-05-11 19:58:47 -0400

    More jokers+
    Added Archy, D'or, Eulenspiegul, Pellesini, Mistake, Anchor, Furbo E Stupedo, Mummer, Anagraph, Beanstalk, Corpse Paint, Founding Father, Pygalion, Homemade Comic, and Test Card (Added them in reverse order)

--- Items/Jokers/founding_father.lua
+++ Items/Jokers/founding_father.lua
@@ -14,9 +14,22 @@ local founding_father = {
     discovered = false,
     blueprint_compat = false,
     eternal_compat = true,
+
+    in_pool = function(self, args)
+        if G.deck then
+            if #G.deck.cards > 0 then
+                for i = 1, #G.deck.cards do
+                    if G.deck.cards[i].config.center == G.P_CENTERS["m_aij_charged"] then
+                        return true
+                    end
+                end
+            end
+        end
+        return false
+    end,
   
     loc_vars = function(self, info_queue, card)
-  
+        info_queue[#info_queue + 1] = G.P_CENTERS.m_aij_charged
     end,
   
     calculate = function(self, card, context)
--- Items/Jokers/pygmalion.lua
+++ Items/Jokers/pygmalion.lua
@@ -1,22 +1,35 @@
 local pygmalion = {
     object_type = "Joker",
-    order = 278,
-    ignore = true,
+    order = 280,
+
     key = "pygmalion",
     config = {
-      
+       
     },
-    rarity = 1,
+    rarity = 2,
     pos = { x = 1, y = 11},
     atlas = 'joker_atlas',
-    cost = 4,
+    cost = 6,
     unlocked = true,
-    discovered = true,
+    discovered = false,
     blueprint_compat = false,
-    eternal_compat = false,
+    eternal_compat = true,
+
+    in_pool = function(self, args)
+        if G.deck then
+            if #G.deck.cards > 0 then
+                for i = 1, #G.deck.cards do
+                    if G.deck.cards[i].config.center == G.P_CENTERS["m_stone"] then
+                        return true
+                    end
+                end
+            end
+        end
+        return false
+    end,
   
     loc_vars = function(self, info_queue, card)
-  
+        info_queue[#info_queue + 1] = G.P_CENTERS.m_stone
     end,
   
     calculate = function(self, card, context)
