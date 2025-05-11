dadf97219e01910e327c4f485e604fc27b71780a
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-05-10 23:00:07 -0400

    Broken Fate was fixed + New Tarot
    Broken Fate is fixed (guess its just Fate now), Added Tarot for Charged Cards "Isis Urania"

--- Items/Consumables/Tarots/Other/broken_fate.lua
+++ Items/Consumables/Tarots/Other/broken_fate.lua
@@ -20,8 +20,46 @@ local broken_fate = {
     use = function(self, card)
 		local ran_amount = 0.75 + math.random(0, math.floor((2.5 - 0.75) / 0.05 + 0.5)) * 0.05
 		local string = "X"..tostring(ran_amount)
-        jest_ability_calculate(G.consumeables.cards[1],"*", ran_amount, nil ,nil, true, "ability.extra")
-		card_eval_status_text(card, 'extra', nil, nil, nil, {message = string, colour = G.C.FILTER})
+        G.consumeables.cards[1].ability.jest_broken_fate_applied = {}
+		table.insert(G.consumeables.cards[1].ability.jest_broken_fate_applied, ran_amount)
+        jest_ability_calculate(G.consumeables.cards[1],"*", ran_amount, nil ,nil, true, false, "ability")
+		card_eval_status_text(G.consumeables.cards[1], 'extra', nil, nil, nil, {message = string, colour = G.C.FILTER})
     end,
 }
+local use_consumeable_ref = Card.use_consumeable
+function Card:use_consumeable(area, copier)
+  if self.ability and self.ability.jest_broken_fate_applied and #self.ability.jest_broken_fate_applied > 0 then
+    for i = 1, #self.ability.jest_broken_fate_applied do
+        jest_ability_calculate(
+        self,
+        "/", self.ability.jest_broken_fate_applied[i],
+        nil,
+        nil,   
+        true,  
+        false,    
+        "ability"  
+        )
+    end
+  end
+
+  return use_consumeable_ref(self, area, copier)
+end
+local sell_card_ref = Card.sell_card
+function Card:sell_card()
+  if self.ability and self.ability.jest_broken_fate_applied and #self.ability.jest_broken_fate_applied > 0 then
+    for i = 1, #self.ability.jest_broken_fate_applied do
+        jest_ability_calculate(
+        self,
+        "/", self.ability.jest_broken_fate_applied[i],
+        nil,
+        nil,   
+        true,  
+        false,    
+        "ability"  
+        )
+    end
+  end
+
+  return sell_card_ref(self)
+end
 return {name = {"Tarots"}, items = {broken_fate}}
