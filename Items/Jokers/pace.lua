8781a6fb2c31d57f052e9526f7a1c73fca9fd1c2
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-05-11 10:40:58 -0400

    Pace fix
    Pace no longer has lasting side effects when winning/return to menu

--- Items/Jokers/pace.lua
+++ Items/Jokers/pace.lua
@@ -25,10 +25,24 @@ local updateref = Card.update
 function Card:update(dt)
   local ref = updateref(self, dt)
 
+  if self.config.center.jest_real_rarity ~= nil then
+      local orig = self.config.center.jest_real_rarity
+      if orig == "2" or orig == "3" or orig == "Uncommon" or orig == "Rare" then
+        local num = tonumber(orig)
+        if num then
+          self.config.center.rarity = num
+        else
+          self.config.center.rarity = orig
+        end
+      end
+  end
+
   if G.jokers and self.ability.set == 'Joker' then
-    if self.ability.jest_real_rarity == nil then
-      local r = self.config.center.rarity
-      self.ability.jest_real_rarity = type(r) == "number" and tostring(r) or r
+    if self.config.center.jest_real_rarity ~= "1" and self.config.center.jest_real_rarity ~= "Common" then
+        if self.config.center.jest_real_rarity == nil then
+          local r = self.config.center.rarity
+          self.config.center.jest_real_rarity = type(r) == "number" and tostring(r) or r
+        end
     end
 
     local has_pace = false
@@ -38,25 +52,81 @@ function Card:update(dt)
         break
       end
     end
-
-    if has_pace then
-      local orig = self.ability.jest_real_rarity
-      if orig == "2" or orig == "3" or orig == "Uncommon" or orig == "Rare" then
-        self.config.center.rarity = 1
-      end
-    else
-      local orig = self.ability.jest_real_rarity
-      local num = tonumber(orig)
-      if num then
-        self.config.center.rarity = num
-      else
-        self.config.center.rarity = orig
-      end
-
-      self.ability.jest_real_rarity = nil
+    if self.config.center.jest_real_rarity ~= "1" and self.config.center.jest_real_rarity ~= "Common" then
+        if has_pace then
+          local orig = self.config.center.jest_real_rarity
+          if orig == "2" or orig == "3" or orig == "Uncommon" or orig == "Rare" then
+            self.config.center.rarity = 1
+          end
+        else
+          local orig = self.config.center.jest_real_rarity
+          local num = tonumber(orig)
+          if num then
+            self.config.center.rarity = num
+          else
+            self.config.center.rarity = orig
+          end
+        end
     end
+  elseif self.ability.set == 'Joker' then
+      if self.config.center.jest_real_rarity ~= "1" and self.config.center.jest_real_rarity ~= "Common" then
+          if self.config.center.jest_real_rarity == nil then
+              local r = self.config.center.rarity
+              self.config.center.jest_real_rarity = type(r) == "number" and tostring(r) or r
+          end
+
+          local orig = self.config.center.jest_real_rarity
+          local num = tonumber(orig)
+          if num then
+            self.config.center.rarity = num
+          else
+            self.config.center.rarity = orig
+          end
+      end
   end
 
+  return ref
+end
+
+local main_menuref = Game.main_menu
+function Game:main_menu(change_context)
+  for k, joker in pairs(G.P_CENTER_POOLS['Joker']) do
+      if joker.config.center and joker.config.center.jest_real_rarity ~= nil then
+          if joker.config.center.jest_real_rarity ~= "1" and joker.config.center.jest_real_rarity ~= "Common" then
+              local orig = joker.config.center.jest_real_rarity
+              local num = tonumber(orig)
+              if num then
+                joker.config.center.rarity = num
+              else
+                joker.config.center.rarity = orig
+              end
+              joker.config.center.jest_real_rarity = nil
+          end
+      end
+  end
+
+  local ref = main_menuref(self, change_context)
+  return ref
+end
+
+local start_runref = Game.start_run
+function Game:start_run(args)
+  for k, joker in pairs(G.P_CENTER_POOLS['Joker']) do
+      if joker.config.center and joker.config.center.jest_real_rarity ~= nil then
+          if joker.config.center.jest_real_rarity ~= "1" and joker.config.center.jest_real_rarity ~= "Common" then
+              local orig = joker.config.center.jest_real_rarity
+              local num = tonumber(orig)
+              if num then
+                joker.config.center.rarity = num
+              else
+                joker.config.center.rarity = orig
+              end
+              joker.config.center.jest_real_rarity = nil
+          end
+      end
+  end
+
+  local ref = start_runref(self, args)
   return ref
 end
 
