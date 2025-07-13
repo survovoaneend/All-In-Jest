1403b91440615488dd669eb188b2a3a5aaf09035
Aiden Anderson <aidenanderson08@outlook.com>
2025-07-13 18:17:54 -0500

    tetrominoker and croupier

--- Items/Jokers/croupier.lua
+++ Items/Jokers/croupier.lua
@@ -9,14 +9,14 @@ local croupier = {
         }
     },
     rarity = 1,
-    pos = { x = 23, y = 12},
+    pos = { x = 23, y = 12 },
     atlas = 'joker_atlas',
     cost = 4,
     unlocked = true,
     discovered = false,
     blueprint_compat = true,
     eternal_compat = true,
-  
+
     loc_vars = function(self, info_queue, card)
         return {
             vars = {
@@ -24,36 +24,37 @@ local croupier = {
             }
         }
     end,
-  
+
     calculate = function(self, card, context)
         if context.jest_scored_chips and not context.blueprint then
-            card.ability.extra.total_chips = tostring(tonumber(card.ability.extra.total_chips) + context.jest_scored_chips)
-        end
-
-        if context.end_of_round and not context.individual and not context.repetition and not context.blueprint then
-            if tonumber(card.ability.extra.total_chips) > (to_big(G.GAME.blind.chips) * 2) then
-                return {
-                    card = card,
-                    dollars = card.ability.extra.money,
-                }
-            end
+        local current_hand_score = to_number(context.jest_scored_chips)
+        print(current_hand_score)
+        if current_hand_score > (to_number(G.GAME.blind.chips) * 2) then
+            return {
+                dollars = card.ability.extra.money, 
+                card = card,
+            }
         end
     end
-  
+    end
+
 }
 local evaluate_playref = G.FUNCS.evaluate_play
 function G.FUNCS.evaluate_play(self, e)
     evaluate_playref(self, e)
 
     for i = 1, #G.jokers.cards do
-        local effects = eval_card(G.jokers.cards[i], {
-            card = G.consumeables,
-            after = true,
-            jest_scored_chips = to_big(hand_chips * mult)
-        })
-        if effects.jokers then
-            card_eval_status_text(G.jokers.cards[i], "jokers", nil, 0.3, nil, effects.jokers)
+        if G.jokers.cards[i].config.center_key == 'j_aij_croupier' then
+            local effects = eval_card(G.jokers.cards[i], {
+                card = G.consumeables,
+                after = true,
+                jest_scored_chips = to_big(hand_chips * mult)
+            })
+            if effects.jokers then
+                card_eval_status_text(G.jokers.cards[i], "jokers", nil, 0.3, nil, effects.jokers)
+            end
         end
     end
 end
-return { name = {"Jokers"}, items = {croupier} }
+
+return { name = { "Jokers" }, items = { croupier } }
--- Items/Jokers/tetrominoker.lua
+++ Items/Jokers/tetrominoker.lua
@@ -26,40 +26,25 @@ local tetrominoker = {
     end,
   
     calculate = function(self, card, context)
-        if context.individual and context.cardarea == G.play then
+        if context.individual and context.cardarea == G.play and not context.blueprint then
             if context.other_card:get_id() == 4 then
                 if pseudorandom('tetrominoker') < G.GAME.probabilities.normal / card.ability.extra.odds then
-                    G.playing_card = (G.playing_card and G.playing_card + 1) or 1
-                    local _card = copy_card(context.other_card, nil, nil, G.playing_card)
-                    _card:add_to_deck()
-                    G.deck.config.card_limit = G.deck.config.card_limit + 1
-                    table.insert(G.playing_cards, _card)
-                    card.is_duplicating = true 
-                    playing_card_joker_effects({ _card })
-                    card.is_duplicating = false
-                    G.hand:emplace(_card)
-                    _card.states.visible = nil
-                    G.E_MANAGER:add_event(Event({
-                      trigger = 'after',
-                      delay = 0.7,
-                      func = function()
-                          if _card and not _card.REMOVED then
-                              _card:start_materialize()
-                              return true
-                          end
-                          return true
-                      end
-                    }))
-
-                    card:juice_up(0.4, 0.3)
-
-                    return {
-                  
-                      message = localize('k_copied_ex'),
-                      colour = G.C.FILTER,
-                      card = card,
-                    }
-                end
+                        G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+                local copied_card = copy_card(context.other_card, nil, nil, G.playing_card)
+                copied_card:add_to_deck()
+                G.deck.config.card_limit = G.deck.config.card_limit + 1
+                table.insert(G.playing_cards, copied_card)
+                G.hand:emplace(copied_card)
+                copied_card.states.visible = nil
+                
+                G.E_MANAGER:add_event(Event({
+                    func = function() 
+                        copied_card:start_materialize()
+                        return true
+                    end
+                }))
+                        card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "Copied", colour = G.C.GREEN})
+                    end
             end
         end
     end
