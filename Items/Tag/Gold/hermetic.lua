2736d09433d283bb3f75943e8fb675d4d7b96a6e
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-07-24 13:18:40 -0400

    Select Polish

--- Items/Tag/Gold/hermetic.lua
+++ Items/Tag/Gold/hermetic.lua
@@ -19,23 +19,28 @@ local hermetic_tag = {
         if context.type == 'immediate' then
             tag:jest_apply("+", G.C.ATTENTION, function()
                 if #G.consumeables.cards < G.consumeables.config.card_limit then
-				    G.SETTINGS.paused = true
-                    G.FUNCS.overlay_menu{
-                        config = {no_esc = true},
-                        definition = SMODS.jest_no_back_card_collection_UIBox(
-                            G.P_CENTER_POOLS.Spectral, 
-                            {4,5}, 
-                            {
-                                no_materialize = true, 
-                                modify_card = function(card, center) 
-                                    if card.config.center.discovered and (not card.config.center.hidden) then
-                                        jest_create_select_card_ui(card, G.consumeables)
-                                    end
-                                end, 
-                                h_mod = 1.05,
+                    G.E_MANAGER:add_event(Event({
+                        func = function() 
+				            G.SETTINGS.paused = true
+                            G.FUNCS.overlay_menu{
+                                config = {no_esc = true},
+                                definition = SMODS.jest_no_back_card_collection_UIBox(
+                                    G.P_CENTER_POOLS.Spectral, 
+                                    {4,5}, 
+                                    {
+                                        no_materialize = true, 
+                                        modify_card = function(card, center) 
+                                            if card.config.center.discovered and (not card.config.center.hidden) then
+                                                jest_create_select_card_ui(card, G.consumeables)
+                                            end
+                                        end, 
+                                        h_mod = 1.05,
+                                    }
+                                ),
                             }
-                        ),
-                    }
+                            return true 
+                        end 
+                    }))
                 end
                 return true
 			end,
--- Items/Tag/Gold/nonstandard.lua
+++ Items/Tag/Gold/nonstandard.lua
@@ -66,79 +66,86 @@ local nonstandard_tag = {
                     cur_card:start_dissolve()
                 end
                 temp_card:start_dissolve()
-                G.FUNCS.overlay_menu{
-                    config = {}, 
-                    definition = SMODS.jest_no_back_card_collection_UIBox(
-                        cards.Suits, 
-                        {4}, 
-                        {
-                            no_materialize = true, 
-                            hide_single_page = true,
-                            collapse_single_page = true,
-                            modify_card = function(card, center) 
-                                if card.config.center.discovered then
-                                    for k, v in pairs(SMODS.Suits) do
-                                        if v.key == center.order then
-                                            local new_code = SMODS.Suits[v.key].card_key
-                                            local new_val = SMODS.Ranks['Ace'].card_key
-                                            local new_card = G.P_CARDS[new_code..'_'..new_val]
-
-                                            card:set_base(new_card)
-                                        end
-                                    end
-                                    jest_create_select_playing_card_ui(card, G.deck, {
-                                        following = 4,
-                                        pools = {
-                                            cards.Ranks,
-                                            G.P_CENTER_POOLS.Enhanced,
-                                            G.P_CENTER_POOLS.Seal,
-                                            G.P_CENTER_POOLS.Edition,
-                                        },
-                                        size = {
-                                            x = {6,4,5,5}, 
-                                            y = {6,4,5,5}
-                                        },
-                                        copies = 2,
-                                        playing_card = true,
-                                        extra_function = function(extra_card, extra_center, prev_card, extra_data)
-                                            if extra_data.times == 1 then
-                                                for k, v in pairs(SMODS.Ranks) do
-                                                    if v.key == extra_center.order then
-                                                        local new_code = SMODS.Suits[prev_card.base.suit].card_key
-                                                        local new_val = SMODS.Ranks[v.key].card_key
-                                                        local new_card = G.P_CARDS[new_code..'_'..new_val]
-
-                                                        extra_card:set_base(new_card)
-                                                    end
-                                                end
-                                            end
-                                            if extra_data.times >= 2 then
-                                                for k, v in pairs(G.P_CENTER_POOLS.Enhanced) do
-                                                    local new_code = SMODS.Suits[prev_card.base.suit].card_key
-                                                    local new_val = SMODS.Ranks[prev_card.base.value].card_key
+                G.E_MANAGER:add_event(Event({
+                    func = function() 
+                        G.SETTINGS.paused = true
+                        G.FUNCS.overlay_menu{
+                            config = {}, 
+                            definition = SMODS.jest_no_back_card_collection_UIBox(
+                                cards.Suits, 
+                                {4}, 
+                                {
+                                    no_materialize = true, 
+                                    hide_single_page = true,
+                                    collapse_single_page = true,
+                                    modify_card = function(card, center) 
+                                        if card.config.center.discovered then
+                                            for k, v in pairs(SMODS.Suits) do
+                                                if v.key == center.order then
+                                                    local new_code = SMODS.Suits[v.key].card_key
+                                                    local new_val = SMODS.Ranks['Ace'].card_key
                                                     local new_card = G.P_CARDS[new_code..'_'..new_val]
 
-                                                    extra_card:set_base(new_card)
+                                                    card:set_base(new_card)
                                                 end
-                                                extra_data.center = 'c_base'
                                             end
-                                            if extra_data.times == 3 then
-                                                extra_card:set_ability(G.P_CENTERS[prev_card.config.center.key])
-                                                extra_card:set_seal(extra_center.key, true)
-                                            end
-                                            if extra_data.times == 4 then
-                                                extra_card:set_ability(G.P_CENTERS[prev_card.config.center.key])
-                                                extra_card:set_seal(prev_card.seal, true)
-                                                if extra_center.discovered then extra_card:set_edition(extra_center.key, true, true) end
-                                            end
-                                        end,
-                                    })
-                                end
-                            end, 
-                            h_mod = 1.05,
+                                            jest_create_select_playing_card_ui(card, G.deck, {
+                                                following = 4,
+                                                pools = {
+                                                    cards.Ranks,
+                                                    G.P_CENTER_POOLS.Enhanced,
+                                                    G.P_CENTER_POOLS.Seal,
+                                                    G.P_CENTER_POOLS.Edition,
+                                                },
+                                                size = {
+                                                    x = {6,4,5,5}, 
+                                                    y = {6,4,5,5}
+                                                },
+                                                copies = 2,
+                                                playing_card = true,
+                                                extra_function = function(extra_card, extra_center, prev_card, extra_data)
+                                                    if extra_data.times == 1 then
+                                                        for k, v in pairs(SMODS.Ranks) do
+                                                            if v.key == extra_center.order then
+                                                                local new_code = SMODS.Suits[prev_card.base.suit].card_key
+                                                                local new_val = SMODS.Ranks[v.key].card_key
+                                                                local new_card = G.P_CARDS[new_code..'_'..new_val]
+
+                                                                extra_card:set_base(new_card)
+                                                            end
+                                                        end
+                                                    end
+                                                    if extra_data.times >= 2 then
+                                                        for k, v in pairs(G.P_CENTER_POOLS.Enhanced) do
+                                                            local new_code = SMODS.Suits[prev_card.base.suit].card_key
+                                                            local new_val = SMODS.Ranks[prev_card.base.value].card_key
+                                                            local new_card = G.P_CARDS[new_code..'_'..new_val]
+
+                                                            extra_card:set_base(new_card)
+                                                        end
+                                                        extra_data.center = 'c_base'
+                                                    end
+                                                    if extra_data.times == 3 then
+                                                        extra_card:set_ability(G.P_CENTERS[prev_card.config.center.key])
+                                                        extra_card:set_seal(extra_center.key, true)
+                                                    end
+                                                    if extra_data.times == 4 then
+                                                        extra_card:set_ability(G.P_CENTERS[prev_card.config.center.key])
+                                                        extra_card:set_seal(prev_card.seal, true)
+                                                        if extra_center.discovered then extra_card:set_edition(extra_center.key, true, true) end
+                                                        extra_card.ability.jest_copy_edition = true
+                                                    end
+                                                end,
+                                            })
+                                        end
+                                    end, 
+                                    h_mod = 1.05,
+                                }
+                            ),
                         }
-                    ),
-                }
+                        return true 
+                    end 
+                }))
                 return true
             end)
 
