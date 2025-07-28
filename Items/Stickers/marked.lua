7b0e8080128a16f2d8b45f60b71cc6340cdb26cc
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-07-28 15:37:18 -0400

    Most of the pit blinds

---
+++ Items/Stickers/marked.lua
@@ -0,0 +1,14 @@
+local marked = {
+    object_type = "Sticker",
+    key = "marked",
+	no_collection = true,
+	config = { extra = {  } },
+	atlas = "stickers_atlas",
+	order = 1,
+	pos = {x = 0, y = 0},
+	badge_colour = HEX('d95763'),
+	loc_vars = function(self, info_queue, card)
+		return { vars = {}}
+	end,
+}
+return {name = {"Stickers"}, items = {marked}}
---
+++ Items/Vouchers/gold_medal.lua
@@ -0,0 +1,21 @@
+local gold_medal = {
+  object_type = "Voucher",
+  key = 'gold_medal',
+  config = {
+    extra = {
+      rate = 4
+    }
+  },
+  atlas = 'vouchers_atlas',
+  pos = { x = 2, y = 0 },
+  discovered = false,
+
+  loc_vars = function(self, info_queue)
+    return {vars = {self.config.extra.rate}}
+  end,
+
+  redeem = function(self)
+    G.GAME.jest_gold_tag_rate = self.config.extra.rate
+  end
+}
+return {name = "Vouchers", items = {gold_medal}}
