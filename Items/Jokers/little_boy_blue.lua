b57c7f18e926dd5fc8d3b2401fe28054a8601dec
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-08 23:39:26 -0400

    Some jokers and stuff

--- Items/Jokers/little_boy_blue.lua
+++ Items/Jokers/little_boy_blue.lua
@@ -1,28 +1,59 @@
 local little_boy_blue = {
     object_type = "Joker",
     order = 129,
-    ignore = true,
 
     key = "little_boy_blue",
     config = {
-      
+      extra = {
+          chips = 200,
+          banned_cards = {}
+      }
     },
-    rarity = 1,
+    rarity = 3,
     pos = { x = 23, y = 4 },
     atlas = 'joker_atlas',
-    cost = 4,
+    cost = 8,
     unlocked = true,
-    discovered = true,
+    discovered = false,
     blueprint_compat = false,
-    eternal_compat = false,
+    eternal_compat = true,
   
     loc_vars = function(self, info_queue, card)
-  
+        return {
+            vars = {
+                card.ability.extra.chips,
+            }
+        }
     end,
   
     calculate = function(self, card, context)
-      
-    end
-  
+        if context.joker_main then
+            return {
+                chips = card.ability.extra.chips
+            }
+        end
+    end,
+
+    add_to_deck = function(self, card, from_debuff)
+        if #SMODS.find_card("j_aij_little_boy_blue") <= 0 then
+            for k, joker in pairs(G.P_CENTER_POOLS['Joker']) do
+                local joker = G.P_CENTER_POOLS['Joker'][k]
+                local key = joker.key
+                local text = retrieve_joker_text(key, true)
+                if (text:find('Mult') or text:find('mult')) or not (text:find('Chip') or text:find('chip')) then
+                    G.GAME.banned_keys[key] = true
+                    table.insert(card.ability.extra.banned_cards, key)
+                end
+            end
+        end
+    end,
+
+    remove_from_deck = function(self, card, from_debuff)
+        if #SMODS.find_card("j_aij_little_boy_blue") <= 0 then
+		    for _, key in ipairs(card.ability.extra.banned_cards) do
+                G.GAME.banned_keys[key] = nil
+            end
+        end
+	end,
 }
 return { name = {"Jokers"}, items = {little_boy_blue} }
