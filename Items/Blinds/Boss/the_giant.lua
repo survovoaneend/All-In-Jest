a92f524a998fab7645356b3a3094ef296563e6c8
Aiden Anderson <aidenanderson08@outlook.com>
2025-08-11 17:47:01 -0500

    new blind

---
+++ Items/Blinds/Boss/the_giant.lua
@@ -0,0 +1,20 @@
+local the_giant = {
+    object_type = "Blind",
+    key = 'the_giant',
+    boss = {
+        min = 3,
+    },
+    mult = 2,
+    boss_colour = HEX("7a73bb"),
+    atlas = 'blinds',
+    pos = { X = 0, y = 47},
+    order = 26,
+    dollars = 5,
+
+
+    calculate = function(self, card, context)
+        
+    end,
+
+}
+return { name = {"Blinds"}, items = {the_giant} }
