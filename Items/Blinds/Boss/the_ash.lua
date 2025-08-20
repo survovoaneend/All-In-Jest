5adf434c77a1ad7b923920a8f94946973a709df6
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-19 23:11:59 -0400

    Rest of the blinds

--- Items/Blinds/Boss/the_ash.lua
+++ Items/Blinds/Boss/the_ash.lua
@@ -1,7 +1,6 @@
 local the_ash = {
     object_type = "Blind",
     key = 'the_ash',
-    ignore = true,
     boss = {
         min = 5,
     },
@@ -12,10 +11,54 @@ local the_ash = {
     order = 17,
     dollars = 5,
 
-
-    calculate = function(self, card, context)
-        
+    loc_vars = function(self)
+        return {
+            vars = {G.GAME.all_in_jest.starting_prams.deck_size}
+        }
     end,
 
+    collection_loc_vars = function(self)
+        return {
+            vars = {'[Starting size]'}
+        }
+    end,
+
+    set_blind = function(self)
+        local extra_mult = 0
+        if #G.deck.cards <= G.GAME.all_in_jest.starting_prams.deck_size then
+            extra_mult = (G.GAME.all_in_jest.starting_prams.deck_size - #G.deck.cards) * 0.2
+        end
+        local amt = G.GAME.blind.mult
+        G.GAME.blind.mult = G.GAME.blind.mult + extra_mult
+        G.GAME.blind.chips = G.GAME.blind.chips/amt
+        G.GAME.blind.chips = G.GAME.blind.chips*G.GAME.blind.mult
+        G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+    end,
+
+    disable = function(self)
+        local amt = G.GAME.blind.mult
+        G.GAME.blind.chips = G.GAME.blind.chips/amt
+        G.GAME.blind.chips = G.GAME.blind.chips*2
+        G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+    end,
+
+    defeat = function(self)
+        local temp = G.GAME.blind and G.GAME.blind.disabled
+        if temp then
+            return
+        end
+        if not temp then
+            local amt = G.GAME.blind.mult
+            G.GAME.blind.chips = G.GAME.blind.chips/amt
+            G.GAME.blind.chips = G.GAME.blind.chips*2
+            G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+        end
+    end,
+
+    in_pool = function(self)
+        if G.deck and #G.deck.cards <= G.GAME.all_in_jest.starting_prams.deck_size then
+            return true
+        end
+    end,
 }
 return { name = {"Blinds"}, items = {the_ash} }
--- Items/Blinds/Boss/the_oak.lua
+++ Items/Blinds/Boss/the_oak.lua
@@ -1,7 +1,6 @@
 local the_oak = {
     object_type = "Blind",
     key = 'the_oak',
-    ignore = true,
     boss = {
       min = 3,
     },
@@ -12,8 +11,43 @@ local the_oak = {
     order = 7,
     dollars = 5,
 
+    recalc_debuff = function(self, card, from_blind)
+        local temp = G.GAME.blind and G.GAME.blind.disabled
+        if temp then
+            return false
+        end
+        if card.area == G.jokers and #G.jokers > 0 and card == G.jokers[#G.jokers] and not temp then
+            return true
+        end
+        return false
+    end,
+
     calculate = function(self, card, context)
-        
+        local temp = G.GAME.blind and G.GAME.blind.disabled
+        if temp then
+            return
+        end
+        if G.jokers and #G.jokers.cards > 0 and not temp then
+            for k, v in pairs(G.jokers.cards) do
+                if v == G.jokers.cards[#G.jokers.cards] then
+                    SMODS.debuff_card(v, true, 'the_oak')
+                else
+                    SMODS.debuff_card(v, false, 'the_oak')
+                end
+            end
+        end
+    end,
+
+    disable = function(self)
+        for k, v in pairs(G.jokers.cards) do
+            SMODS.debuff_card(v, false, 'the_oak')
+        end
+    end,
+
+    defeat = function(self)
+        for k, v in pairs(G.jokers.cards) do
+            SMODS.debuff_card(v, false, 'the_oak')
+        end
     end
 }
 return { name = {"Blinds"}, items = {the_oak} }
