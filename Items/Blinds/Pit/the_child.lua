92f7e02e4180cc1fc5294047b48f0f3f9798bb02
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-21 15:18:24 -0400

    The Child Polish

--- Items/Blinds/Pit/the_child.lua
+++ Items/Blinds/Pit/the_child.lua
@@ -28,15 +28,23 @@ local the_child = {
         end
 
         if context.after and context.scoring_hand and not temp then
+            for i=1, #context.scoring_hand do
+                local percent = 1.15 - (i-0.999)/(#context.scoring_hand-0.998)*0.3
+                G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() context.scoring_hand[i]:flip();play_sound('card1', percent);context.scoring_hand[i]:juice_up(0.3, 0.3);return true end }))
+            end
             G.E_MANAGER:add_event(Event({
                 trigger = 'before',
                 delay = 1.5,
                 func = (function()
+                    local index = 1
                     for k, v in pairs(context.scoring_hand) do
+                        local percent = 0.85 + (index-0.999)/(#context.scoring_hand-0.998)*0.3
                         if v:get_id() ~= 2 then
-                            v:juice_up(1, 0.5)
                             assert(SMODS.modify_rank(v, -1))
                         end
+                        v:flip()
+                        play_sound('tarot2', percent, 0.6)
+                        v:juice_up(0.3, 0.3)
                     end
                     return true 
                 end
