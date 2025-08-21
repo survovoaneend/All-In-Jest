2aa955bff28b412569ba55687980c670a2b2e9ac
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-21 13:54:22 -0400

    Jokers
    Joculator, Lexicon

--- Items/Jokers/joculator.lua
+++ Items/Jokers/joculator.lua
@@ -1,27 +1,43 @@
 local joculator = {
   object_type = "Joker",
   order = 174,
-  ignore = true,
 
   key = "joculator",
   config = {
-    
+    extra = {
+        hand_amount = 1,
+    }
   },
-  rarity = 1,
+  rarity = 2,
   pos = { x = 17, y = 6},
   atlas = 'joker_atlas',
-  cost = 4,
+  cost = 6,
   unlocked = true,
   discovered = false,
   blueprint_compat = false,
-  eternal_compat = false,
+  eternal_compat = true,
 
   loc_vars = function(self, info_queue, card)
-
+      return {
+            vars = {
+                card.ability.extra.hand_amount,
+            }
+      }
   end,
 
   calculate = function(self, card, context)
-    
+      if context.hand_drawn and G.GAME.current_round.discards_left == 0 and G.GAME.current_round.hands_left == card.ability.extra.hand_amount then
+          for i = 1, G.hand.config.card_limit do
+              if #G.deck.cards > 0 then
+                  G.E_MANAGER:add_event(Event({
+                      func = function()
+                      draw_card(G.deck,G.hand, 90,'up', nil)
+                      return true
+                      end
+                  }))
+              end
+          end
+      end
   end
 }
 return { name = {"Jokers"}, items = {joculator} }
--- Items/Jokers/lexicon.lua
+++ Items/Jokers/lexicon.lua
@@ -1,25 +1,50 @@
 local lexicon = {
     object_type = "Joker",
     order = 322,
-    ignore = true,
     key = "lexicon",
     config = {
     },
-    rarity = 1,
+    rarity = 3,
     pos = { x = 18, y = 12},
     atlas = 'joker_atlas',
-    cost = 4,
+    cost = 8,
     unlocked = true,
-    discovered = true,
-    blueprint_compat = false,
-    eternal_compat = false,
+    discovered = false,
+    blueprint_compat = true,
+    eternal_compat = true,
   
     loc_vars = function(self, info_queue, card)
-
+        local mult = 0
+        if G.jokers then
+            for i = 1, #G.jokers.cards do
+                if G.jokers.cards[i] ~= card and G.jokers.cards[i].config.center.key ~= 'j_aij_lexicon' then
+                    local text = retrieve_joker_text(G.jokers.cards[i])
+                    for num in string.gmatch(text, "%d+") do
+                        mult = mult + tonumber(num)  
+                    end
+                end
+            end
+        end
+        return { vars = {mult}}
     end,
   
     calculate = function(self, card, context)
-        
+        if context.joker_main and G.jokers then
+            local mult = 0
+            for i = 1, #G.jokers.cards do
+                if G.jokers.cards[i] ~= card and G.jokers.cards[i].config.center.key ~= 'j_aij_lexicon' then
+                    local text = retrieve_joker_text(G.jokers.cards[i])
+                    for num in string.gmatch(text, "%d+") do
+                        mult = mult + tonumber(num)  
+                    end
+                end
+            end
+            if mult ~= 0 then
+                return {
+                    mult = mult
+                }
+            end
+        end
     end
   
 }
