1b51917183cd3f8f9343073f521dbeceabe12f95
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-22 00:24:36 -0400

    Ijoker.co

--- Items/Jokers/ijoker_co.lua
+++ Items/Jokers/ijoker_co.lua
@@ -1,27 +1,38 @@
 local ijoker_co = {
     object_type = "Joker",
     order = 211,
-    ignore = true,
 
     key = "ijoker_co",
     config = {
-      
+      extra = {
+          tags = 2
+      }
     },
-    rarity = 1,
+    rarity = 2,
     pos = { x = 0, y = 8},
     atlas = 'joker_atlas',
-    cost = 4,
+    cost = 6,
     unlocked = true,
     discovered = false,
     blueprint_compat = false,
-    eternal_compat = false,
+    eternal_compat = true,
   
     loc_vars = function(self, info_queue, card)
-  
+        return {
+            vars = {
+                card.ability.extra.tags,
+            }
+        }
     end,
   
     calculate = function(self, card, context)
-      
+        if context.entering_shop then
+            for i = 1, card.ability.extra.tags do
+                local price = math.random(5, 10)
+                local tag = get_next_tag_key()
+                All_in_Jest.add_tag_to_shop(tag, price)
+            end
+        end
     end
   
 }
