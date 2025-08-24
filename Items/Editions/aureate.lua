aad0a8fdb0dff1e42316f74aec44886df86817e5
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-24 12:00:06 -0400

    Aureate

---
+++ Items/Editions/aureate.lua
@@ -0,0 +1,50 @@
+local aureate_shader = {
+    object_type = "Shader",
+    key = 'aureate', 
+    path = 'aureate.fs',
+}
+SMODS.Sound {
+  key = 'aureate',
+  path = 'glimmer.mp3'
+}
+local aureate = {
+    object_type = "Edition",
+    key = 'aureate', 
+    sound = {
+        sound = 'aij_aureate',
+        per = 1,
+        vol = 1
+    },
+    order = 3,
+    config = { 
+        money = 0.3,
+        max = 30
+    },
+    loc_vars = function(self, info_queue, card)
+        local money = ((card.edition or {}).money or self.config.money) + 1
+        return {
+            vars = {
+                money,
+                (card.edition or {}).max or self.config.max
+            }
+        }
+    end,
+    calculate = function(self, card, context)
+		if context.post_joker or (context.main_scoring and context.cardarea == G.play) then
+            local money = ((card.edition or {}).money or self.config.money) + 1
+            local max = (card.edition or {}).max or self.config.max
+			return {
+                dollars = math.floor(math.min((G.GAME.dollars*money)-G.GAME.dollars, max)),
+            }
+		end
+	end,
+    in_shop = true,
+    weight = 3,
+    extra_cost = 10,
+    get_weight = function(self)
+        return self.weight
+    end,
+
+    shader = 'aureate'
+}
+return {name = "Editions", items = {aureate, aureate_shader}}
