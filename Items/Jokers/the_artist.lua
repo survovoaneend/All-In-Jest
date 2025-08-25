4cb98b2ba057d773a2add62e8019d594d14b3be1
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-25 07:12:47 -0400

    The Artist Fix

--- Items/Jokers/the_artist.lua
+++ Items/Jokers/the_artist.lua
@@ -49,6 +49,50 @@ local the_artist = {
                 end
             end
         end
+        if G.shop_jokers and G.shop_jokers.cards then 
+            for i = 1, #G.shop_jokers.cards do
+                if G.shop_jokers.cards[i] and G.shop_jokers.cards[i].config.center.set == 'Tarot' then
+                    local index = 0
+                    for j = 1, #SMODS.find_card("j_aij_the_artist") do 
+                        if SMODS.find_card("j_aij_the_artist")[j] == card then
+                            index = j
+                        end
+                    end
+                    
+                    if not G.shop_jokers.cards[i].ability.jest_the_artist_applied or (G.shop_jokers.cards[i].ability.jest_the_artist_applied and not G.shop_jokers.cards[i].ability.jest_the_artist_applied[index]) then
+                        G.shop_jokers.cards[i].ability.jest_the_artist_applied = G.shop_jokers.cards[i].ability.jest_the_artist_applied or {}
+                        G.shop_jokers.cards[i].ability.jest_the_artist_applied[index] = card.ability.extra.more_selection
+                        card.ability.extra.index = tostring(index)
+                        if G.shop_jokers.cards[i].ability.max_highlighted then
+                            jest_ability_calculate(G.shop_jokers.cards[i],"+", card.ability.extra.more_selection, nil ,nil, true, false, "ability.consumeable")
+                            G.shop_jokers.cards[i].ability.max_highlighted = G.shop_jokers.cards[i].ability.max_highlighted + card.ability.extra.more_selection
+                        end
+                    end
+                end
+            end
+        end
+        if G.pack_cards and G.pack_cards.cards then
+            for i = 1, #G.pack_cards.cards do
+                if G.pack_cards.cards[i] and G.pack_cards.cards[i].config.center.set == 'Tarot' then
+                    local index = 0
+                    for j = 1, #SMODS.find_card("j_aij_the_artist") do 
+                        if SMODS.find_card("j_aij_the_artist")[j] == card then
+                            index = j
+                        end
+                    end
+                    
+                    if not G.pack_cards.cards[i].ability.jest_the_artist_applied or (G.pack_cards.cards[i].ability.jest_the_artist_applied and not G.pack_cards.cards[i].ability.jest_the_artist_applied[index]) then
+                        G.pack_cards.cards[i].ability.jest_the_artist_applied = G.pack_cards.cards[i].ability.jest_the_artist_applied or {}
+                        G.pack_cards.cards[i].ability.jest_the_artist_applied[index] = card.ability.extra.more_selection
+                        card.ability.extra.index = tostring(index)
+                        if G.pack_cards.cards[i].ability.max_highlighted then
+                            jest_ability_calculate(G.pack_cards.cards[i],"+", card.ability.extra.more_selection, nil ,nil, true, false, "ability.consumeable")
+                            G.pack_cards.cards[i].ability.max_highlighted = G.pack_cards.cards[i].ability.max_highlighted + card.ability.extra.more_selection
+                        end
+                    end
+                end
+            end
+        end
     end,
 
     remove_from_deck = function(self, card, from_debuff)
@@ -72,6 +116,54 @@ local the_artist = {
                             end
                         end
                         G.consumeables.cards[i].ability.max_highlighted = G.consumeables.cards[i].ability.max_highlighted - card.ability.extra.more_selection
+                    end
+                end
+            end
+        end
+        if G.shop_jokers and G.shop_jokers.cards then 
+            for i = 1, #G.shop_jokers.cards do
+                if G.shop_jokers.cards[i] and G.shop_jokers.cards[i].config.center.set == 'Tarot' then
+                    if G.shop_jokers.cards[i].ability and G.shop_jokers.cards[i].ability.jest_the_artist_applied and G.shop_jokers.cards[i].ability.consumeable.max_highlighted then
+                        for k, v in pairs(G.shop_jokers.cards[i].ability.jest_the_artist_applied) do
+                            if k == tonumber(card.ability.extra.index) then
+                                if v then
+                                    jest_ability_calculate(
+                                    G.shop_jokers.cards[i],
+                                    "-", v,
+                                    nil,
+                                    nil,   
+                                    true,  
+                                    false,    
+                                    "ability.consumeable"  
+                                    )
+                                end
+                            end
+                        end
+                        G.shop_jokers.cards[i].ability.max_highlighted = G.shop_jokers.cards[i].ability.max_highlighted - card.ability.extra.more_selection
+                    end
+                end
+            end
+        end
+        if G.pack_cards and G.pack_cards.cards then
+            for i = 1, #G.pack_cards.cards do
+                if G.pack_cards.cards[i] and G.pack_cards.cards[i].config.center.set == 'Tarot' then
+                    if G.pack_cards.cards[i].ability and G.pack_cards.cards[i].ability.jest_the_artist_applied and G.pack_cards.cards[i].ability.consumeable.max_highlighted then
+                        for k, v in pairs(G.pack_cards.cards[i].ability.jest_the_artist_applied) do
+                            if k == tonumber(card.ability.extra.index) then
+                                if v then
+                                    jest_ability_calculate(
+                                    G.pack_cards.cards[i],
+                                    "-", v,
+                                    nil,
+                                    nil,   
+                                    true,  
+                                    false,    
+                                    "ability.consumeable"  
+                                    )
+                                end
+                            end
+                        end
+                        G.pack_cards.cards[i].ability.max_highlighted = G.pack_cards.cards[i].ability.max_highlighted - card.ability.extra.more_selection
                     end
                 end
             end
