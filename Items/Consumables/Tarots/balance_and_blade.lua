53b998d02383219fd81b7948beb5dc636ddbe55d
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-25 20:27:40 -0400

    The Balance and the Blade Polish

--- Items/Consumables/Tarots/balance_and_blade.lua
+++ Items/Consumables/Tarots/balance_and_blade.lua
@@ -18,13 +18,43 @@ local balance_and_blade = {
 		}
 	end,
 	use = function(self, card)
+		local index = 0
+		local cur_suit
 		local keys = {}
-		for k, v in pairs(SMODS.Suits) do
-			keys[#keys+1] = k
+		for key, val in pairs(SMODS.Suits) do
+			local temp_tally = 0
+			for k, v in pairs(G.hand.highlighted) do
+				if v.base.suit ~= key and not All_in_Jest.has_patches(v, key) then
+					temp_tally = temp_tally + 1
+				end
+			end 
+			if temp_tally == #G.hand.highlighted then
+				keys[#keys+1] = key
+			end
 		end
-		local cur_suit = pseudorandom_element(keys, pseudoseed('balance_and_blade'))
-		for i=1, #G.hand.highlighted do
-			All_in_Jest.add_patch(G.hand.highlighted[i], cur_suit);
+		cur_suit = pseudorandom_element(keys, pseudoseed('balance_and_blade'))
+		if #keys <= 0 then
+			keys = {}
+			for key, val in pairs(SMODS.Suits) do
+				for k, v in pairs(G.hand.highlighted) do
+					if v.base.suit ~= key and not All_in_Jest.has_patches(v, key) then
+						keys[#keys+1] = key
+					end
+				end 
+			end
+			cur_suit = pseudorandom_element(keys, pseudoseed('balance_and_blade_resample'))
+			if #keys <= 0 then
+				keys = {}
+				for key, val in pairs(SMODS.Suits) do
+					keys[#keys+1] = key
+				end
+				cur_suit = pseudorandom_element(keys, pseudoseed('balance_and_blade_resample_2'))
+			end
+		end
+		for k, v in pairs(G.hand.highlighted) do
+			if v.base.suit ~= cur_suit and not All_in_Jest.has_patches(v, cur_suit) then
+				All_in_Jest.add_patch(v, cur_suit);
+			end
         end 
 	end,
 }
