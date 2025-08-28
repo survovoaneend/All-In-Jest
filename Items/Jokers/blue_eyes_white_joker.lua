a0b7c98a764bd36591b43238516a5c663fb43517
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-28 16:59:27 -0400

    SMODS Built in Retrigger System

--- Items/Jokers/blue_eyes_white_joker.lua
+++ Items/Jokers/blue_eyes_white_joker.lua
@@ -21,7 +21,7 @@ local blue_eyes_white_joker = {
     calculate = function(self, card, context)
         if context.after and context.full_hand and G.GAME.current_round.hands_left == 0 then
             if #context.full_hand == 1 then
-                context.full_hand[1].ability.perma_retriggers = context.full_hand[1].ability.perma_retriggers + card.ability.extra.retriggers
+                context.full_hand[1].ability.perma_repetitions = context.full_hand[1].ability.perma_repetitions + card.ability.extra.retriggers
                 return {
                     extra = {message = localize('k_upgrade_ex'), focus = context.full_hand[1], colour = G.C.FILTER},
                     colour = G.C.FILTER,
