fbb76e5aa4fb9b855a48a9f939c07158b687f4cd
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-28 16:16:27 -0400

    Saltimbanco Nerf

--- Items/Jokers/saltimbanco.lua
+++ Items/Jokers/saltimbanco.lua
@@ -3,7 +3,9 @@ local saltimbanco = {
     order = 108,
     key = "saltimbanco",
     config = {
-      
+      extra = {
+          trigger = false
+      }
     },
     rarity = 1,
     pos = { x = 3, y = 4 },
@@ -19,10 +21,16 @@ local saltimbanco = {
     end,
   
     calculate = function(self, card, context)
-        if context.pseudorandom_result and G.play ~= nil and G.play.cards ~= nil and G.play.cards[1].highlighted then
+        if context.after then
+            card.ability.extra.trigger = false
+        end
+        if context.initial_scoring_step then
+            card.ability.extra.trigger = true
+        end
+        if context.pseudorandom_result and G.play ~= nil and G.play.cards ~= nil and card.ability.extra.trigger then
             if context.numerator > 0 and context.denominator > 0 and #G.play.cards > 0 then
                 local xmlt = 1 + (1 - (context.numerator/context.denominator))
-                if context.trigger_obj and (context.trigger_obj.config and context.trigger_obj.config.center and (context.trigger_obj.config.center.set == 'Joker' or context.trigger_obj.config.center.set == 'Default' or context.trigger_obj.config.center.set == 'Enhanced' or context.trigger_obj.config.center.consumeable)) then
+                if context.result and context.trigger_obj and (context.trigger_obj.config and context.trigger_obj.config.center and (context.trigger_obj.config.center.set == 'Joker' or context.trigger_obj.config.center.set == 'Default' or context.trigger_obj.config.center.set == 'Enhanced' or context.trigger_obj.config.center.consumeable)) and xmlt > 1 then
                     return {
                         message = localize{type='variable',key='a_xmult',vars={xmlt}},
                         Xmult_mod = xmlt,
@@ -31,6 +39,5 @@ local saltimbanco = {
             end
         end
     end
-  
 }
 return { name = {"Jokers"}, items = {saltimbanco} }
