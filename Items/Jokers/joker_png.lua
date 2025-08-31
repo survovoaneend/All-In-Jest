f3edbe5cdd3bffb110f0a08d43179f2c2e35afcf
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-08-31 13:27:10 -0400

    Add files via upload

--- Items/Jokers/joker_png.lua
+++ Items/Jokers/joker_png.lua
@@ -1,7 +1,6 @@
 local joker_png = {
     object_type = "Joker",
     order = 302,
-    ignore = true,
     key = "joker_png",
     config = {
       
@@ -11,16 +10,33 @@ local joker_png = {
     atlas = 'joker_atlas',
     cost = 4,
     unlocked = true,
-    discovered = true,
+    discovered = false,
     blueprint_compat = false,
     eternal_compat = false,
+
+    pixel_size = { w = 31, h = 40 },
   
     loc_vars = function(self, info_queue, card)
-  
+        
     end,
   
     calculate = function(self, card, context)
-        
+        if context.setting_blind then
+            local jokers = {}
+            for k,v in pairs(G.P_CENTER_POOLS["Joker"]) do
+                if v.discovered and v.rarity ~= 4 then
+                    if v.in_pool and type(v.in_pool) == 'function' then
+                        if v:in_pool() then
+                            jokers[#jokers+1] = v
+                        end
+                    else
+                        jokers[#jokers+1] = v
+                    end
+                end
+            end
+            local joker_center = pseudorandom_element(jokers, pseudoseed('joker_png'))
+            All_in_Jest.reroll_joker(card, joker_center.key, 'joker_png', card.config.center.key)
+        end
     end
   
 }
