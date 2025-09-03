53a8b1f264cbf57e1334f73daff786cb253441a6
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-09-02 21:05:15 -0400

    Smiley Fix

--- Items/Seals/smiley.lua
+++ Items/Seals/smiley.lua
@@ -14,29 +14,29 @@ local smiley = {
         return { vars = { card.ability.seal.extra.joker_amt } }
     end,
     calculate = function(self, card, context)
-        if context.remove_playing_cards then
-            local trigger = false
-            for k, v in pairs(context.removed) do
-                if v == card then
-                    trigger = true
-                end
-            end
-            if trigger then
-                for i = 1, card.ability.seal.extra.joker_amt do
-                    local jokers = {}
-                    for i = 1, #G.jokers.cards do
-                        if G.jokers.cards[i].edition == nil then
-                            table.insert(jokers, G.jokers.cards[i])
-                        end
-                    end
-                    local joker = pseudorandom_element(jokers, pseudoseed('jest_anarchy_tag'))
-                    if joker and #jokers > 0 then
-                        local edition = poll_edition('smiley'..G.GAME.round_resets.ante, 2, false, true)
-                        joker:set_edition(edition)
-                    end
-                end
-            end
-        end
+        
     end
 }
+local start_dissolve_ref = Card.start_dissolve
+function Card:start_dissolve(dissolve_colours, silent, dissolve_time_fac, no_juice)
+  local ref = start_dissolve_ref(self, dissolve_colours, silent, dissolve_time_fac, no_juice)
+  if G.jokers then
+    if self.seal and self.seal == 'aij_smiley' then
+      for i = 1, self.ability.seal.extra.joker_amt do
+          local jokers = {}
+          for i = 1, #G.jokers.cards do
+              if G.jokers.cards[i].edition == nil then
+                  table.insert(jokers, G.jokers.cards[i])
+              end
+          end
+          local joker = pseudorandom_element(jokers, pseudoseed('jest_anarchy_tag'))
+          if joker and #jokers > 0 then
+              local edition = poll_edition('smiley'..G.GAME.round_resets.ante, 2, false, true)
+              joker:set_edition(edition)
+          end
+      end
+    end
+  end
+  return ref
+end
 return {name = {"Seals"}, items = {smiley}}
