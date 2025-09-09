661310a97686a0705649f25a534ee50da0e24fee
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-09-09 10:17:25 -0400

    The Field Fix

--- Items/Blinds/Boss/the_field.lua
+++ Items/Blinds/Boss/the_field.lua
@@ -10,14 +10,14 @@ local the_field = {
     pos = { X = 0, y = 20 },
     order = 12,
     dollars = 5,
-    config = { extra = { cards = 0 } },
+    config = { extra = { cards = 0, defeated = false } },
 
     calculate = function(self, card, context)
         local temp = G.GAME.blind and G.GAME.blind.disabled
-        if temp then
+        if temp or self.config.extra.defeated then
             return
         end
-        if context.discard and not temp then
+        if context.discard and not temp and not self.config.extra.defeated then
             self.config.extra.cards = self.config.extra.cards + 1    
             if self.config.extra.cards >= 10 then
                 for k, v in pairs(G.playing_cards) do
@@ -25,7 +25,7 @@ local the_field = {
                 end
             end
         end
-        if self.config.extra.cards < 10 and not temp then
+        if self.config.extra.cards < 10 and not temp and not self.config.extra.defeated then
             for k, v in pairs(G.playing_cards) do
                 SMODS.debuff_card(v, true, 'the_field')
             end
@@ -39,6 +39,7 @@ local the_field = {
     end,
 
     defeat = function(self)
+        self.config.extra.defeated = true
         for k, v in pairs(G.playing_cards) do
             SMODS.debuff_card(v, false, 'the_field')
         end
