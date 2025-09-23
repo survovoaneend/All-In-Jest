2533cfdc8bafbf96cad19f04f67caf29169783bf
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-09-23 12:58:49 +1000

    Adds nicer animation to Silver Screen Prevents adding silver to destroyed cards

--- Items/Jokers/silver_screen.lua
+++ Items/Jokers/silver_screen.lua
@@ -21,19 +21,43 @@ local silver_screen = {
   
     calculate = function(self, card, context)
         if context.remove_playing_cards then
-            for k, v in pairs(context.removed) do
-                if v:is_face() then
-                    local hand_cards = {}
-                    for i = 1, #G.hand.cards do
-                        if G.hand.cards[i].edition == nil then
-                            table.insert(hand_cards, G.hand.cards[i])
+            local hand_cards = {}
+            for i = 1, #G.hand.cards do
+                if G.hand.cards[i].edition == nil then
+                    local to_be_destroyed = false
+                    for k, v in pairs(context.removed) do
+                        if G.hand.cards[i] == v then
+                            to_be_destroyed = true
                         end
                     end
-                    local playing_card = pseudorandom_element(hand_cards, pseudoseed('jest_silver_screen'))
-                    local edition = {aij_silver = true}
-                    card:juice_up(0.3, 0.5)
-                    playing_card:set_edition(edition)
+                    if not to_be_destroyed then
+                        table.insert(hand_cards, G.hand.cards[i])
+                    end
                 end
+            end
+            
+            local enhanced = 0
+            for k, v in pairs(context.removed) do
+                if v:is_face() then
+                    enhanced = enhanced + 1
+
+                    local playing_card, chosen_index = pseudorandom_element(hand_cards, pseudoseed('jest_silver_screen'))
+                    table.remove(hand_cards, chosen_index) -- Should prevent one card being enhanced twice
+                    local edition = {aij_silver = true}
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            card:juice_up(0.3, 0.5)
+                            playing_card:set_edition(edition, true)
+                            return true
+                        end
+                    }))
+                end
+            end
+            if enhanced > 0 then
+                return {
+                    message = localize("aij_silver", "labels") .. "!",
+                    colour = G.C.GREY
+                }
             end
         end
     end
