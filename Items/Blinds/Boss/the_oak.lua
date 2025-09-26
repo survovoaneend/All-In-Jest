f5c94ffed7ab7c197f631efe7f750319f2b23c69
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-09-26 01:01:52 -0400

    The Oak x Greasepaint Fix

--- Items/Blinds/Boss/the_oak.lua
+++ Items/Blinds/Boss/the_oak.lua
@@ -16,7 +16,13 @@ local the_oak = {
         if temp then
             return false
         end
+        if card.ability.aij_the_oak_debuff then
+            SMODS.debuff_card(card, false, 'the_oak')
+            card.ability.aij_the_oak_debuff = false
+        end
         if card.area == G.jokers and #G.jokers > 0 and card == G.jokers[#G.jokers] and not temp then
+            SMODS.debuff_card(card, true, 'the_oak')
+            card.ability.aij_the_oak_debuff = true
             return true
         end
         return false
@@ -31,8 +37,38 @@ local the_oak = {
             for k, v in pairs(G.jokers.cards) do
                 if v == G.jokers.cards[#G.jokers.cards] then
                     SMODS.debuff_card(v, true, 'the_oak')
-                else
+                    v.ability.aij_the_oak_debuff = true
+                elseif v.ability.aij_the_oak_debuff then
                     SMODS.debuff_card(v, false, 'the_oak')
+                    v.ability.aij_the_oak_debuff = false
+                end
+            end
+        end
+    end,
+
+    update = function(self, blind, dt)
+        if blind then
+            local temp = blind and blind.disabled
+            if temp then
+                if G.jokers and #G.jokers.cards > 0 then
+                    for k, v in pairs(G.jokers.cards) do
+                        if v.ability.aij_the_oak_debuff then
+                            SMODS.debuff_card(v, false, 'the_oak')
+                            v.ability.aij_the_oak_debuff = false
+                        end
+                    end
+                end
+                return
+            end
+            if G.jokers and #G.jokers.cards > 0 and not temp then
+                for k, v in pairs(G.jokers.cards) do
+                    if v == G.jokers.cards[#G.jokers.cards] then
+                        SMODS.debuff_card(v, true, 'the_oak')
+                        v.ability.aij_the_oak_debuff = true
+                    elseif v.ability.aij_the_oak_debuff then
+                        SMODS.debuff_card(v, false, 'the_oak')
+                        v.ability.aij_the_oak_debuff = false
+                    end
                 end
             end
         end
