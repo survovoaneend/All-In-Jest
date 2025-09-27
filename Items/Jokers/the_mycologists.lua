6b9432d68c8fe7d99e11bb163ccd3c6cb90cf007
Aiden Anderson <aidenanderson08@outlook.com>
2025-09-27 12:46:21 -0500

    The Mycologists blueprint stuff

--- Items/Jokers/the_mycologists.lua
+++ Items/Jokers/the_mycologists.lua
@@ -13,7 +13,7 @@ local the_mycologists = {
     cost = 8,
     unlocked = true,
     discovered = false,
-    blueprint_compat = false,
+    blueprint_compat = true,
     eternal_compat = true,
   
     loc_vars = function(self, info_queue, card)
