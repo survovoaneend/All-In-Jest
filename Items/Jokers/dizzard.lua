51c1992e2972571226ce96689b64c79ff8250ab3
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-09-27 14:28:21 -0400

    Move Dizzard Code

--- Items/Jokers/dizzard.lua
+++ Items/Jokers/dizzard.lua
@@ -20,41 +20,7 @@ local dizzard = {
   end,
 
   calculate = function(self, card, context)
-    if context.skip_blind or (context.ending_booster and G.GAME.all_in_jest.dizzard_shop) then
-        stop_use()
-            G.deck:shuffle('cashout'..G.GAME.round_resets.ante)
-            G.deck:hard_set_T()
-            G.GAME.current_round.reroll_cost_increase = 0
-            G.GAME.current_round.used_packs = {}
-            G.GAME.current_round.free_rerolls = G.GAME.round_resets.free_rerolls
-            calculate_reroll_cost(true)
-            if context.skip_blind then
-                G.blind_prompt_box:get_UIE_by_ID('prompt_dynatext1').config.object.pop_delay = 0
-                G.blind_prompt_box:get_UIE_by_ID('prompt_dynatext1').config.object:pop_out(5)
-                G.blind_prompt_box:get_UIE_by_ID('prompt_dynatext2').config.object.pop_delay = 0
-                G.blind_prompt_box:get_UIE_by_ID('prompt_dynatext2').config.object:pop_out(5) 
-            end
-            delay(0.3)
-            G.E_MANAGER:add_event(Event({
-              trigger = 'after',
-              func = function()
-                  if G.blind_select and context.skip_blind then 
-                    G.blind_select:remove()
-                    G.blind_prompt_box:remove()
-                    G.blind_select = nil
-                  end
-                  G.STATE = G.STATES.SHOP
-                  G.GAME.shop_free = nil
-                  G.GAME.shop_d6ed = nil
-                  G.STATE_COMPLETE = false
-                  G.GAME.all_in_jest.dizzard_shop = true
-                return true
-              end
-            }))
-    end
-    if context.ending_shop then
-        G.GAME.all_in_jest.dizzard_shop = false
-    end
+    
   end
 }
 return { name = {"Jokers"}, items = {dizzard} }
