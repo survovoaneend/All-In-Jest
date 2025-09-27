b19dc7ba51cd1cafbe539e6db0c00b0601603646
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-09-27 16:26:04 -0400

    Planetarium Effect & Crash Fix

--- Items/Jokers/planetarium.lua
+++ Items/Jokers/planetarium.lua
@@ -27,30 +27,36 @@ local planetarium = {
     end,
   
     calculate = function(self, card, context)
-        if context.using_consumeable then
-            if context.consumeable.ability.set == 'Planet' and context.consumeable.ability.extra_planetarium_mult then
-                card_eval_status_text(card, 'extra', nil, nil, nil, {message = localize{type='variable',key='a_mult',vars={card.ability.extra.mult}}, colour = G.C.MULT})
+        if context.joker_main and context.scoring_name then
+            local chips = 0
+            local mult = 0
+            if G.consumeables and #G.consumeables.cards > 0 then
+                for k, v in pairs(G.consumeables.cards) do
+                    if v.ability.set == 'Planet' and v.config.center.config.hand_type and context.scoring_name == v.config.center.config.hand_type then
+                        if v.config.center.config.mult then
+                            mult = mult + card.ability.extra.mult
+                        end
+                        if v.config.center.config.chips then
+		                    chips = chips + card.ability.extra.chips
+                        end
+                    end
+                end
             end
-            if context.consumeable.ability.set == 'Planet' and context.consumeable.ability.extra_planetarium_chips then
-		        card_eval_status_text(card, 'extra', nil, nil, nil, {message = localize{type='variable',key='a_chips',vars={card.ability.extra.chips}}, colour = G.C.CHIPS})
+            if chips > 0 and mult > 0 then
+                return {
+                    mult = mult,
+                    chips = chips
+                }
+            elseif chips > 0 then
+                return {
+                    chips = chips
+                }
+            elseif mult > 0 then
+                return {
+                    mult = mult
+                }
             end
         end
     end,
-
-    update = function(self, card, dt)
-        if G.consumeables then
-            for i = 1, #G.consumeables.cards do
-                if G.consumeables.cards[i].config.center.config.mult and (not G.consumeables.cards[i].ability.extra_planetarium_mult or #G.consumeables.cards[i].ability.extra_planetarium_mult == 0 or not G.consumeables.cards[i].ability.extra_planetarium_mult[card])then
-                    G.consumeables.cards[i].ability.extra_planetarium_mult = G.consumeables.cards[i].ability.extra_planetarium_mult or {} 
-                    G.consumeables.cards[i].ability.extra_planetarium_mult[card] = card.ability.extra.mult
-                end
-                if G.consumeables.cards[i].config.center.config.chips and (not G.consumeables.cards[i].ability.extra_planetarium_chips or #G.consumeables.cards[i].ability.extra_planetarium_chips == 0 or not G.consumeables.cards[i].ability.extra_planetarium_chips[card]) then
-                    G.consumeables.cards[i].ability.extra_planetarium_chips = G.consumeables.cards[i].ability.extra_planetarium_chips or {} 
-                    G.consumeables.cards[i].ability.extra_planetarium_chips[card] = card.ability.extra.chips
-                end
-            end
-        end
-    end,
-  
 }
 return { name = {"Jokers"}, items = {planetarium} }
