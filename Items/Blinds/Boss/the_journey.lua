c08f451c76c2dee095e02ea167493e334f73109c
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-10-01 19:33:35 -0400

    The Journey Rework

--- Items/Blinds/Boss/the_journey.lua
+++ Items/Blinds/Boss/the_journey.lua
@@ -3,7 +3,7 @@ local the_journey = {
     key = 'the_journey',
     boss = {
         min = 3,
-        trigger = false
+        selected_suit = '[X]',
     },
     mult = 2,
     boss_colour = HEX("fdd879"),
@@ -11,25 +11,51 @@ local the_journey = {
     pos = { X = 0, y = 56},
     order = 28,
     dollars = 5,
+    config = {
+        extra = {
+            trigger = false,
+        }
+    },
 
+    loc_vars = function(self)
+        local ability = G.GAME.blind.ability
+        if self.boss.selected_suit == '[X]' then
+            self.boss.selected_suit = pseudorandom_element(All_in_Jest.get_suits('key'), pseudoseed('the_journey'))
+        end
+        return {
+            vars = {ability and ability.selected_suit or self.boss.selected_suit}
+        }
+    end,
+
+    collection_loc_vars = function(self)
+        return {
+            vars = {'[X]'}
+        }
+    end,
 
     calculate = function(self, card, context)
         local temp = G.GAME.blind and G.GAME.blind.disabled
         if temp then
             return
         end
-        if context.end_of_round and not self.boss.trigger and not temp then
-            G.E_MANAGER:add_event(Event({
-                func = function()
-                    G.GAME.win_ante = G.GAME.win_ante + 1
-                    return true
-                end
-            }))
-            self.boss.trigger = true
+        local ability = G.GAME.blind.ability
+        if ability and context.individual and context.cardarea == G.play and not temp then
+            if context.other_card:is_suit(self.boss.selected_suit) and not ability.trigger then
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        G.GAME.win_ante = G.GAME.win_ante + 1
+                        return true
+                    end
+                }))
+                ability.trigger = true
+            end
         end
     end,
+    defeat = function(self)
+        self.boss.selected_suit = '[X]'
+    end,
     in_pool = function(self)
-        if G.GAME.round_resets.ante < 7 and not G.GAME.won then
+        if G.GAME.round_resets.ante < (G.GAME.win_ante-1) and not G.GAME.won then
             return true
         end
     end,
