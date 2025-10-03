3dc401f61ab0785084adb6d88d97831469b202c0
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-10-03 20:54:30 +1000

    Fixed Blood Artist with glass and duplicates

--- Items/Jokers/blood_artist.lua
+++ Items/Jokers/blood_artist.lua
@@ -4,9 +4,9 @@ local blood_artist = {
 
     key = "blood_artist",
     config = {
-      extra = {
-          blind_reduction = 10
-      }
+        extra = {
+            blind_reduction = 10
+        }
     },
     rarity = 2,
     pos = { x = 6, y = 3 },
@@ -16,7 +16,7 @@ local blood_artist = {
     discovered = false,
     blueprint_compat = false,
     eternal_compat = true,
-  
+
     loc_vars = function(self, info_queue, card)
         return {
             vars = {
@@ -24,23 +24,35 @@ local blood_artist = {
             }
         }
     end,
-  
+
     calculate = function(self, card, context)
-      
-    end
-}
-local start_dissolve_ref = Card.start_dissolve
-function Card:start_dissolve(dissolve_colours, silent, dissolve_time_fac, no_juice)
-  local ref = start_dissolve_ref(self, dissolve_colours, silent, dissolve_time_fac, no_juice)
-  if G.jokers then
-    local has_blood_artist = next(SMODS.find_card("j_aij_blood_artist"))
-    if has_blood_artist then
-        for i = 1, has_blood_artist do
-            G.GAME.blind.chips = G.GAME.blind.chips - math.ceil(G.GAME.blind.chips * (SMODS.find_card("j_aij_blood_artist")[i].ability.extra.blind_reduction * 0.01))
-            G.GAME.blind.chip_text = G.GAME.blind.chips
+        if G.GAME.blind.chips > 0 then
+            -- Removing non-playing cards and selling cards
+            if context.joker_type_destroyed or context.selling_card then
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        G.GAME.blind.chips = G.GAME.blind.chips - math.ceil(G.GAME.blind.chips * (card.ability.extra.blind_reduction * 0.01))
+                        G.GAME.blind.chip_text = G.GAME.blind.chips
+                        return true
+                    end
+                }))
+            end
+
+            -- Removing playing cards
+            if context.remove_playing_cards then
+                local destroyed_count = #context.removed
+                G.E_MANAGER:add_event(Event({
+                    func = function ()
+                        G.GAME.blind.chips = G.GAME.blind.chips - math.ceil(G.GAME.blind.chips * (card.ability.extra.blind_reduction * 0.01) * destroyed_count)
+                        G.GAME.blind.chip_text = G.GAME.blind.chips
+                        return true
+                    end
+                }))
+            end
+
+            -- return nil, true
         end
     end
-  end
-  return ref
-end
-return { name = {"Jokers"}, items = {blood_artist} }
+}
+
+return { name = { "Jokers" }, items = { blood_artist } }
