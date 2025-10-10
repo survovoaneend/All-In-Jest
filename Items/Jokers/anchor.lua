6c155d0d623f422809aecf62640848187b9f1e89
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-10-10 20:45:50 +1000

    anchor no longer blueprint compatible

--- Items/Jokers/anchor.lua
+++ Items/Jokers/anchor.lua
@@ -11,7 +11,7 @@ local anchor = {
     cost = 6,
     unlocked = true,
     discovered = false,
-    blueprint_compat = true,
+    blueprint_compat = false,
     eternal_compat = true,
 
     loc_vars = function(self, info_queue, card)
