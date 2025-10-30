5d8a4e555e01103249bfa15bbd213803060526b4
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-10-30 22:51:51 +1000

    Magic Mirror resets properly now

--- Items/Jokers/magic_mirror.lua
+++ Items/Jokers/magic_mirror.lua
@@ -34,7 +34,7 @@ local magic_mirror = {
   
     calculate = function(self, card, context)
       if context.ante_change and context.ante_change ~= 0 and context.ante_end then
-          G.GAME.jest_magic_mirror_trigger = false
+          G.GAME.jest_magic_mirror_trigger = true
       end
       if context.individual and context.cardarea == G.play then
         if G.GAME.jest_magic_mirror_trigger then
