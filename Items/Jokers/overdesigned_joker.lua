09f271486907e40dc785998f29fd194f4541d1d2
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-10-30 23:46:14 +1000

    Fixed Overdesigned Joker

--- Items/Jokers/overdesigned_joker.lua
+++ Items/Jokers/overdesigned_joker.lua
@@ -1,35 +1,36 @@
 local function jest_overdesigned_joker_cycle(suit)
-    -- BUG: this is unsafe with language changes.
-    -- should have better style: use the actual suit internally, don't localize
-    -- except in loc_vars return
-    if suit == localize('k_aij_overdesigned_heart', 'extra_joker_dictionary') then
-        return {localize('k_aij_overdesigned_club', 'extra_joker_dictionary'), 14}, {localize('k_aij_overdesigned_give_prefix', 'extra_joker_dictionary'), localize('k_aij_youve_got_mail_plus_prefix', 'extra_joker_dictionary'), localize('k_aij_youve_got_mail_mult_text', 'extra_joker_dictionary')}, {G.C.SUITS.Clubs, G.C.MULT}
-    elseif suit == localize('k_aij_overdesigned_club', 'extra_joker_dictionary') then
-        return {localize('k_aij_overdesigned_diamond', 'extra_joker_dictionary'), 2}, {localize('k_aij_overdesigned_earn_prefix', 'extra_joker_dictionary'), localize('$'), ""}, {G.C.SUITS.Diamonds, G.C.MONEY}
-    elseif suit == localize('k_aij_overdesigned_diamond', 'extra_joker_dictionary') then
-        return {localize('k_aij_overdesigned_spade', 'extra_joker_dictionary'), 100}, {localize('k_aij_overdesigned_give_prefix', 'extra_joker_dictionary'), localize('k_aij_youve_got_mail_plus_prefix', 'extra_joker_dictionary'), localize('k_aij_youve_got_mail_chip_text', 'extra_joker_dictionary')}, {G.C.SUITS.Spades, G.C.CHIPS}
-    elseif suit == localize('k_aij_overdesigned_spade', 'extra_joker_dictionary') then
-        return {localize('k_aij_overdesigned_heart', 'extra_joker_dictionary'), 1.5}, {localize('k_aij_overdesigned_give_prefix', 'extra_joker_dictionary'), localize('k_aij_youve_got_mail_xmult_prefix', 'extra_joker_dictionary'), localize('k_aij_youve_got_mail_mult_text', 'extra_joker_dictionary')}, {G.C.SUITS.Hearts, G.C.WHITE, G.C.MULT}
+    if suit == "Hearts" then
+        return {"Clubs", 14}, {"k_aij_overdesigned_give_prefix", "k_aij_youve_got_mail_plus_prefix", "k_aij_youve_got_mail_mult_text"}, {G.C.SUITS.Clubs, G.C.MULT}
+    elseif suit == "Clubs" then
+        return {"Diamonds", 2}, {"k_aij_overdesigned_earn_prefix", "k_aij_youve_got_mail_dollar_prefix", ""}, {G.C.SUITS.Diamonds, G.C.MONEY}
+    elseif suit == "Diamons" then
+        return {"Spades", 100}, {"k_aij_overdesigned_give_prefix", "k_aij_youve_got_mail_plus_prefix", "k_aij_youve_got_mail_chip_text"}, {G.C.SUITS.Spades, G.C.CHIPS}
+    elseif suit == "Spades" then
+        return {"Hearts", 1.5}, {"k_aij_overdesigned_give_prefix", "k_aij_youve_got_mail_xmult_prefix", "k_aij_youve_got_mail_mult_text"}, {G.C.SUITS.Hearts, G.C.WHITE, G.C.MULT}
+    else
+        return {"Clubs", 14}, {"k_aij_overdesigned_give_prefix", "k_aij_youve_got_mail_plus_prefix", "k_aij_youve_got_mail_mult_text"}, {G.C.SUITS.Clubs, G.C.MULT}
     end
 end
+
+
 local overdesigned_joker = {
     object_type = "Joker",
     order = 230,
     
     key = "overdesigned_joker",
     config = {
-      suit = nil,
-      amount = 1.5,
-      extra = {
-          prefix = nil,
-          symbol = nil,
-          suffix = nil,
-          colours = {
-              suit = G.C.SUITS.Hearts,
-              amount = G.C.WHITE,
-              background = G.C.MULT
-          }
-      }
+        suit = "Hearts",
+        amount = 1.5,
+        extra = {
+            prefix = 'k_aij_overdesigned_give_prefix',
+            symbol = 'k_aij_youve_got_mail_xmult_prefix',
+            suffix = 'k_aij_youve_got_mail_mult_text',
+            colours = {
+                suit = G.C.SUITS.Hearts,
+                amount = G.C.WHITE,
+                background = G.C.MULT
+            }
+        }
     },
     rarity = 2,
     pos = { x = 19, y = 8},
@@ -45,19 +46,13 @@ local overdesigned_joker = {
         for _, key in ipairs(suits) do
           info_queue[#info_queue+1] = {set = 'Other', key = key}
         end
-        if not card.ability.extra.prefix then
-            card.ability.suit = localize('k_aij_overdesigned_heart', 'extra_joker_dictionary')
-            card.ability.extra.prefix = localize('k_aij_overdesigned_give_prefix', 'extra_joker_dictionary')
-            card.ability.extra.symbol = localize('k_aij_youve_got_mail_xmult_prefix', 'extra_joker_dictionary')
-            card.ability.extra.suffix = localize('k_aij_youve_got_mail_mult_text', 'extra_joker_dictionary')
-        end
         return {
             vars = {
-                card.ability.suit,
+                localize(card.ability.suit, 'suits_plural'),
                 card.ability.amount,
-                card.ability.extra.prefix,
-                card.ability.extra.symbol,
-                card.ability.extra.suffix,
+                localize(card.ability.extra.prefix, 'extra_joker_dictionary'),
+                localize(card.ability.extra.symbol, 'extra_joker_dictionary'),
+                localize(card.ability.extra.suffix, 'extra_joker_dictionary'),
                 colours = {
                     card.ability.extra.colours.suit,
                     card.ability.extra.colours.amount,
@@ -68,44 +63,78 @@ local overdesigned_joker = {
     end,
   
     calculate = function(self, card, context)
-        if context.individual and context.cardarea == G.play then
-            local cardd = context.other_card
+        if context.individual and context.cardarea == G.play and not context.end_of_round then
+            local other_card = context.other_card
             local orig_suit = card.ability.suit
             local orig_amount = card.ability.amount
+
             if not context.blueprint then
-            local suit_info, suffix_info, colour_info = jest_overdesigned_joker_cycle(card.ability.suit)
-            card.ability.suit = suit_info[1]
-            card.ability.amount = suit_info[2]
-            card.ability.extra.prefix = suffix_info[1]
-            card.ability.extra.symbol = suffix_info[2]
-            card.ability.extra.suffix = suffix_info[3]
-            card.ability.extra.colours.suit = colour_info[1]
-            card.ability.extra.colours.amount = colour_info[2]
-            card.ability.extra.colours.background = colour_info[3] or G.C.WHITE
-            card_eval_status_text(card, 'extra', nil, nil, nil, {message = card.ability.suit, colour = G.C.FILTER})
+                local suit_info, suffix_info, colour_info = jest_overdesigned_joker_cycle(card.ability.suit)
+                card.ability.suit = suit_info[1]
+                card.ability.amount = suit_info[2]
+                card.ability.extra.prefix = suffix_info[1]
+                card.ability.extra.symbol = suffix_info[2]
+                card.ability.extra.suffix = suffix_info[3]
+                card.ability.extra.colours.suit = colour_info[1]
+                card.ability.extra.colours.amount = colour_info[2]
+                card.ability.extra.colours.background = colour_info[3] or G.C.WHITE
             end
-            if cardd:is_suit(orig_suit) then
-                if orig_suit == "Club" then
+
+            -- sendDebugMessage(tprint(other_card), "AIJ")
+            -- sendDebugMessage(orig_suit, "AIJ")
+
+            local juiced_card = context.blueprint_card or card
+            if other_card:is_suit(orig_suit) then
+                if orig_suit == "Clubs" then
                     return {
                         mult = orig_amount,
-                        card = card
+                        card = card,
+                        extra = {
+                            message_card = juiced_card,
+                            message = card.ability.suit,
+                            colour = G.C.FILTER
+                        }
                     }
-                elseif orig_suit == "Diamond" then
+                elseif orig_suit == "Diamonds" then
                     return {
                         dollars = orig_amount,
-                        card = card
+                        card = card,
+                        extra = {
+                            message_card = juiced_card,
+                            message = card.ability.suit,
+                            colour = G.C.FILTER
+                        }
                     }
-                elseif orig_suit == "Spade" then
+                elseif orig_suit == "Spades" then
                     return {
                         chips = orig_amount,
-                        card = card
+                        card = card,
+                        extra = {
+                            message_card = juiced_card,
+                            message = card.ability.suit,
+                            colour = G.C.FILTER
+                        }
                     }
-                elseif orig_suit == "Heart" then
+                elseif orig_suit == "Hearts" then
                     return {
                         x_mult = orig_amount,
-                        card = card
+                        card = card,
+                        extra = {
+                            message_card = juiced_card,
+                            message = card.ability.suit,
+                            colour = G.C.FILTER
+                        }
                     }
                 end
+            else
+                -- Nested in "extra" to stop the playing card from 
+                return {
+                    extra = {
+                        message_card = juiced_card,
+                        message = card.ability.suit,
+                        colour = G.C.FILTER
+                    }
+                }
             end
         end
     end
