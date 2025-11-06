9a306b68648e03f6d004a4e43791f63715b6c028
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-11-06 21:31:25 +1000

    Tool no longer works with suitless cards

--- Items/Jokers/tool.lua
+++ Items/Jokers/tool.lua
@@ -19,7 +19,7 @@ local tool = {
   
     calculate = function(self, card, context)
         if context.change_suit then
-            if context.old_suit ~= context.new_suit and not SMODS.has_enhancement(context.other_card, 'm_aij_canvas') then
+            if not SMODS.has_no_suit(context.other_card) and context.old_suit ~= context.new_suit and not SMODS.has_enhancement(context.other_card, 'm_aij_canvas') then
                 All_in_Jest.add_patch(context.other_card, context.old_suit, true)
                 local juice_card = context.blueprint_card or card
                 juice_card:juice_up()
