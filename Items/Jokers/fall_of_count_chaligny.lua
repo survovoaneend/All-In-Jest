848308645bc4cafab8f8e9e66e984eb703b67b73
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-11-12 15:55:28 +1000

    Animation for fall of count chaligny

--- Items/Jokers/fall_of_count_chaligny.lua
+++ Items/Jokers/fall_of_count_chaligny.lua
@@ -18,10 +18,21 @@ local fall_of_count_chaligny = {
     end,
   
     calculate = function(self, card, context)
-        if context.setting_blind then
-            G.GAME.blind.mult = 1
-            G.GAME.blind.chips = get_blind_amount(G.GAME.round_resets.ante)*G.GAME.blind.mult*G.GAME.starting_params.ante_scaling
-            G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+        if context.setting_blind and not self.getting_sliced and not context.blueprint and G.GAME.blind.mult ~= 1 then
+            G.E_MANAGER:add_event(Event({func = function()
+                G.E_MANAGER:add_event(Event({func = function()
+                    if not card.getting_sliced then
+                        G.GAME.blind.mult = 1
+                        G.GAME.blind.chips = get_blind_amount(G.GAME.round_resets.ante)*G.GAME.blind.mult*G.GAME.starting_params.ante_scaling
+                        G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+                        card:juice_up()
+                        play_sound('timpani')
+                        G.GAME.blind:wiggle()
+                        delay(0.4)
+                    end
+                    return true end }))
+                return true
+            end }))
         end
     end
   
