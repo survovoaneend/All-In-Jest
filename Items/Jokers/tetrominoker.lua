fec02e11f2bb0b3b71a53949e841823184153108
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-11-13 21:54:30 +1000

    Make tetrominoker blueprint compat and have nicer animation

--- Items/Jokers/tetrominoker.lua
+++ Items/Jokers/tetrominoker.lua
@@ -26,25 +26,33 @@ local tetrominoker = {
     end,
   
     calculate = function(self, card, context)
-        if context.individual and context.cardarea == G.play and not context.blueprint then
+        if context.individual and context.cardarea == G.play then
             if context.other_card:get_id() == 4 then
                 if SMODS.pseudorandom_probability(card, 'tetrominoker', 1, card.ability.extra.odds) then
-                        G.playing_card = (G.playing_card and G.playing_card + 1) or 1
-                local copied_card = copy_card(context.other_card, nil, nil, G.playing_card)
-                copied_card:add_to_deck()
-                G.deck.config.card_limit = G.deck.config.card_limit + 1
-                table.insert(G.playing_cards, copied_card)
-                G.hand:emplace(copied_card)
-                copied_card.states.visible = nil
-                
-                G.E_MANAGER:add_event(Event({
-                    func = function() 
-                        copied_card:start_materialize()
-                        return true
-                    end
-                }))
-                        card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "Copied", colour = G.C.GREEN})
-                    end
+                    local copied_card = copy_card(context.other_card, nil, nil, G.playing_card)
+                    copied_card.states.visible = nil
+                    return {
+                        func = function ()
+                            G.E_MANAGER:add_event(Event({
+                                func = function()
+                                    G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+                                    copied_card:add_to_deck()
+                                    G.deck.config.card_limit = G.deck.config.card_limit + 1
+                                    table.insert(G.playing_cards, copied_card)
+                                    G.hand:emplace(copied_card)
+                                    copied_card:start_materialize()
+                                    return true
+                                end
+                            }))
+                        end,
+                        extra = { -- In extra so that the "Copied!" message happens at the same time as the copying
+                            message_card = context.blueprint_card or card,
+                            juice_card = context.other_card,
+                            message = localize('k_copied_ex'),
+                            playing_cards_created = {copied_card}
+                        }
+                    }
+                end
             end
         end
     end
