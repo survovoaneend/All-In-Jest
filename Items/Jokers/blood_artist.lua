4ac86b4582c1b5299e86e5ad73a1711273646d82
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-11-29 23:25:36 +1000

    Polished Blood Artist and made it end round if current score is above the blind requirement

--- Items/Jokers/blood_artist.lua
+++ Items/Jokers/blood_artist.lua
@@ -29,10 +29,16 @@ local blood_artist = {
         if G.GAME.blind.chips > 0 then
             -- Removing non-playing cards and selling cards
             if context.joker_type_destroyed or context.selling_card then
+                All_in_Jest.ease_blind_requirement(nil, -1 * math.ceil(G.GAME.blind.chips * card.ability.extra.blind_reduction * 0.01))
+                -- Ends blind if blind requirement is now met
+                -- Copied from blind:disable()
                 G.E_MANAGER:add_event(Event({
+                    trigger = 'immediate',
                     func = function()
-                        G.GAME.blind.chips = G.GAME.blind.chips - math.ceil(G.GAME.blind.chips * (card.ability.extra.blind_reduction * 0.01))
-                        G.GAME.blind.chip_text = G.GAME.blind.chips
+                        if G.GAME.chips - G.GAME.blind.chips >= 0 then
+                            G.STATE = G.STATES.NEW_ROUND
+                            G.STATE_COMPLETE = false
+                        end
                         return true
                     end
                 }))
@@ -44,10 +50,17 @@ local blood_artist = {
             -- Removing playing cards
             if context.remove_playing_cards then
                 local destroyed_count = #context.removed
+
+                All_in_Jest.ease_blind_requirement(nil, -1 * math.ceil(G.GAME.blind.chips * card.ability.extra.blind_reduction * 0.01) * destroyed_count)
+                -- Ends blind if blind requirement is now met
+                -- Copied from blind:disable()
                 G.E_MANAGER:add_event(Event({
-                    func = function ()
-                        G.GAME.blind.chips = G.GAME.blind.chips - math.ceil(G.GAME.blind.chips * (card.ability.extra.blind_reduction * 0.01) * destroyed_count)
-                        G.GAME.blind.chip_text = G.GAME.blind.chips
+                    trigger = 'immediate',
+                    func = function()
+                        if G.GAME.chips - G.GAME.blind.chips >= 0 then
+                            G.STATE = G.STATES.NEW_ROUND
+                            G.STATE_COMPLETE = false
+                        end
                         return true
                     end
                 }))
