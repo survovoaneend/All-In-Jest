1a5ca4effef3efbd319a473ee91b198af8d3e0ed
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-12-26 13:39:55 -0500

    The Treachery of Jokers

--- Items/Jokers/the_treachery_of_jokers.lua
+++ Items/Jokers/the_treachery_of_jokers.lua
@@ -2,31 +2,34 @@ local the_treachery_of_jokers = {
     object_type = "Joker",
     order = 390,
     key = "the_treachery_of_jokers",
-    ignore = true,
     config = {
         extra = {
-            
+            xmult = 2
         }
     },
-    rarity = 1,
+    rarity = 2,
     pos = { x = 7, y = 16 },
     atlas = 'joker_atlas',
-    cost = 4,
+    cost = 6,
     unlocked = true,
     discovered = false,
     blueprint_compat = false,
-    eternal_compat = false,
+    eternal_compat = true,
 
     loc_vars = function(self, info_queue, card)
         return {
             vars = {
-                
+                card.ability.extra.xmult
             }
         }
     end,
 
     calculate = function(self, card, context)
-        
+         if context.joker_main then
+              return {
+                  xmult = card.ability.extra.xmult
+              }
+         end
     end
 }
 
