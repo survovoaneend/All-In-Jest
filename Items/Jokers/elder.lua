09c69d72f9060bc43e1c7cefdee4470b062e09c6
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-12-29 12:23:21 +1000

    Moved some code into Elder object and fixed Elder freezing stats for silver (will need to port to dev)

--- Items/Jokers/elder.lua
+++ Items/Jokers/elder.lua
@@ -20,11 +20,23 @@ local elder = {
     loc_vars = function(self, info_queue, card)
         local target_joker = All_in_Jest.get_longest_held_joker()
 
-
         if target_joker then
+            local copied_center = target_joker.config.center
+            local info_queue_center = { -- Create a simplified "fake" center that can be used without referencing/modifying the actual center object
+                key = copied_center.key,
+                name = copied_center.name,
+                config = copied_center.config,
+                blueprint_compat = copied_center.blueprint_compat,
+                discovered = true,
+                set = "Joker",
+                create_fake_card = copied_center.create_fake_card,
+                generate_ui = copied_center.generate_ui,
+                loc_vars = copied_center.loc_vars
+            }
+
             local other_vars = nil
-            if target_joker.config.center.loc_vars then
-                local ret = target_joker.config.center:loc_vars({}, target_joker)
+            if copied_center.loc_vars then
+                local ret = copied_center:loc_vars({}, target_joker)
                 if ret then
                     other_vars = ret.vars
                 end
@@ -33,16 +45,35 @@ local elder = {
             end
 
             if other_vars then
-                target_joker.config.center.specific_vars = other_vars
-                target_joker.config.center.specific_vars.aij_elder = target_joker
+                info_queue_center.specific_vars = other_vars
+                info_queue_center.specific_vars.aij_elder = target_joker
             end
 
-            info_queue[#info_queue + 1] = target_joker.config.center
+            info_queue[#info_queue + 1] = info_queue_center
 
-            return { vars = { localize{type="name_text", set="Joker", key = target_joker.config.center.key} } }
+            card.ability.blueprint_compat_ui = card.ability.blueprint_compat_ui or ''
+            card.ability.blueprint_compat_check = nil
+            local main_end = (card.area and card.area == G.jokers) and {
+                {n=G.UIT.C, config={align = "bm", minh = 0.4}, nodes={
+                    {n=G.UIT.C, config={ref_table = card, align = "m", colour = G.C.JOKER_GREY, r = 0.05, padding = 0.06, func = 'blueprint_compat'}, nodes={
+                        {n=G.UIT.T, config={ref_table = card.ability, ref_value = 'blueprint_compat_ui',colour = G.C.UI.TEXT_LIGHT, scale = 0.32*0.8}},
+                    }}
+                }}
+            } or nil
+
+            return { vars = { localize{type="name_text", set="Joker", key = target_joker.config.center.key} }, main_end = main_end }
         end
 
         return { vars = { "None" } }
+    end,
+
+    update = function(self, card, dt)
+        local target_joker = All_in_Jest.get_longest_held_joker()
+        if target_joker and target_joker ~= card and (target_joker.ability.aij_blueprint_compat or target_joker.config.center.blueprint_compat) then
+            card.ability.blueprint_compat = 'compatible'
+        else
+            card.ability.blueprint_compat = 'incompatible'
+        end
     end,
 
     calculate = function(self, card, context)
