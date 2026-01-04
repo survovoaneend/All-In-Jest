89ef17057a5e5c86c14686cd4fadcec4b3a18851
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-01-04 17:21:19 +1000

    Dark Star no longer shows level up for hidden hands

--- Items/Consumables/Planets/dark_star.lua
+++ Items/Consumables/Planets/dark_star.lua
@@ -14,9 +14,7 @@ local dark_star = {
   atlas = 'consumable_atlas',
   loc_vars = function(self, info_queue, card)
     return {
-      vars = {
-
-      },
+      vars = { },
     }
   end,
   can_use = function(self, card)
@@ -32,7 +30,11 @@ local dark_star = {
     local hands_to_upgrade = {}
     for _, k in ipairs(G.handlist) do
       if G.GAME.hands[k].played == 0 then
-        table.insert(hands_to_upgrade, k)
+        if not G.GAME.hands[k].visible then
+            level_up_hand(card, k, true)
+        else
+            table.insert(hands_to_upgrade, k)
+        end
       end
     end
     level_up_hand(card, hands_to_upgrade)
