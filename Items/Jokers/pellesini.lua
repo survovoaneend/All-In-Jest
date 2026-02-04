d3dba23c934c23b9fb1cc7f814cce1b095b4f42e
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-02-04 17:36:08 +1000

    Pellesini no longer cares about joker slots remaining

--- Items/Jokers/pellesini.lua
+++ Items/Jokers/pellesini.lua
@@ -25,23 +25,19 @@ local pellesini = {
         if context.joker_type_destroyed then
             local other_card = context.card
             if other_card.ability.set == "Joker" then
-                if ((#G.jokers.cards + G.GAME.joker_buffer) <= G.jokers.config.card_limit or (context.card.edition ~= nil and context.card.edition.negative)) then
-                    G.GAME.joker_buffer = G.GAME.joker_buffer + 1
-                    local juiced_card = context.blueprint_card or card
-                    G.E_MANAGER:add_event(Event({func = function()
-                        local temp_card = copy_card(other_card, nil, nil, nil, false)
-                        temp_card:add_to_deck()
-                        G.jokers:emplace(temp_card)
-                        temp_card:start_materialize()
-                        if juiced_card == other_card then
-                            temp_card:juice_up(0.6, 0.1)
-                        else
-                            juiced_card:juice_up(0.6, 0.1)
-                        end
-                        G.GAME.joker_buffer = 0
-                        return true
-                    end }))
-                end
+                local juiced_card = context.blueprint_card or card
+                G.E_MANAGER:add_event(Event({func = function()
+                    local temp_card = copy_card(other_card, nil, nil, nil, false)
+                    temp_card:add_to_deck()
+                    G.jokers:emplace(temp_card)
+                    temp_card:start_materialize()
+                    if juiced_card == other_card then
+                        temp_card:juice_up(0.6, 0.1)
+                    else
+                        juiced_card:juice_up(0.6, 0.1)
+                    end
+                    return true
+                end }))
                 return nil, true
             end
         end
