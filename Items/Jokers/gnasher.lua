958345dbef90d9740df8183c4f57946be9db1970
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-02-05 20:05:56 +1000

    Fixed gnasher allowing consumables to create cards outside of consumable slot limit

--- Items/Jokers/gnasher.lua
+++ Items/Jokers/gnasher.lua
@@ -21,9 +21,12 @@ local gnasher = {
         can_use_ability = function(self, card, context)
             local viable_options = {}
             for i, v in ipairs(G.consumeables.cards) do
+                local area = G.consumeables.cards[i].area
+                G.consumeables.cards[i].area = nil
                 if G.consumeables.cards[i]:can_use_consumeable() then
                     viable_options[#viable_options + 1] = i
                 end
+                G.consumeables.cards[i].area = area
             end
             if card.ability.extra.active and #viable_options > 0 then
                 return true
@@ -33,9 +36,12 @@ local gnasher = {
         use_ability = function(self, card)
             local viable_options = {}
             for i, v in ipairs(G.consumeables.cards) do
+                local area = G.consumeables.cards[i].area
+                G.consumeables.cards[i].area = nil
                 if G.consumeables.cards[i]:can_use_consumeable() then
                     viable_options[#viable_options + 1] = i
                 end
+                G.consumeables.cards[i].area = area
             end
             local copied_consumeable = copy_card(G.consumeables.cards[pseudorandom_element(viable_options, pseudoseed('gnasher_consumeable'))])
 
