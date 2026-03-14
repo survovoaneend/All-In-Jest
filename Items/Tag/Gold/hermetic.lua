444f9ca793883a2cc8adc1becbec2a6fd70d1371
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-03-15 09:01:29 +1000

    Non-tag card selection effects work better with tags

--- Items/Tag/Gold/hermetic.lua
+++ Items/Tag/Gold/hermetic.lua
@@ -48,6 +48,16 @@ local hermetic_tag = {
                 return true
               end
             }))
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                for i = 1, #G.GAME.tags do
+                    if G.GAME.tags[i]:apply_to_run({ type = 'new_blind_choice' }) then
+                    break
+                    end
+                end
+                return true
+                end
+            }))
           end
           return true
         end,
