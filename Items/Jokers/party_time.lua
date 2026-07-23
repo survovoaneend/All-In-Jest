3398db0030ea9d81279a9c49e97c3a76b16b8535
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-07-23 09:22:15 -0400

    Party Time! x The Treachery of Jokers Compact

--- Items/Jokers/party_time.lua
+++ Items/Jokers/party_time.lua
@@ -26,7 +26,7 @@ local party_time = {
     end,
 
     calculate = function(self, card, context)
-        if context.end_of_round and context.main_eval and G.jokers and #G.jokers.cards > 0 then
+        if context.end_of_round and context.main_eval then
             card_eval_status_text(card, 'extra', nil, nil, nil, {
                 message = localize('k_aij_party_time'),
                 colour = G.C.FILTER
@@ -38,6 +38,17 @@ local party_time = {
                     message = localize('k_upgrade_ex'),
                     colour = G.C.CHIPS
                 })
+            end
+            -- For The Treachery of Jokers
+            for k, v in pairs(G.consumeables.cards) do
+                if v.config.center_key and v.ability.set == 'Joker' then
+                    v.ability.perma_bonus = v.ability.perma_bonus or 0
+                    v.ability.perma_bonus = v.ability.perma_bonus + card.ability.extra.chip_mod
+                    card_eval_status_text(v, 'extra', nil, nil, nil, {
+                        message = localize('k_upgrade_ex'),
+                        colour = G.C.CHIPS
+                    })
+                end
             end
         end
     end
