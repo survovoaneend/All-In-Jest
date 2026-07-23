260fbb974db271710426782250b0f470242619c4
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-07-23 13:08:51 +1000

    Cinema and Omnichrome now apply to editioned jokers

--- Items/Tag/Gold/omnichrome.lua
+++ Items/Tag/Gold/omnichrome.lua
@@ -1,51 +1,54 @@
 local omnichrome_tag = {
-    object_type = "Tag",
-    key = 'omnichrome',
+  object_type = "Tag",
+  key = 'omnichrome',
 
-    pos = { x = 9, y = 3 },
-    atlas = 'tag_atlas',
-    config = {
-        aij = {
-            upgrade = 'polychrome',
-        }
-    },
+  pos = { x = 9, y = 3 },
+  atlas = 'tag_atlas',
+  config = {
+    aij = {
+      upgrade = 'polychrome',
+    }
+  },
 
-    discovered = false,
-    order = 6,
-    min_ante = 3,
+  discovered = false,
+  order = 6,
+  min_ante = 3,
 
-    loc_vars = function(self, info_queue)
-    end,
+  loc_vars = function(self, info_queue)
+  end,
 
-    apply = function(self, tag, context)
-        if context.type == 'immediate' then
-            tag:jest_apply("+", G.C.ATTENTION, function()
-                local jokers = {}
-                for i = 1, #G.jokers.cards do
-                    if G.jokers.cards[i].edition == nil then
-                        table.insert(jokers, G.jokers.cards[i])
-                    end
+  apply = function(self, tag, context)
+    if context.type == 'new_blind_choice' then
+      tag:jest_apply("+", G.C.ATTENTION, 
+      function()
+          if #G.jokers.cards > 0 then
+            local joker = G.jokers.cards[#G.jokers.cards]
+            local edition = { polychrome = true }
+            joker:set_edition(edition, true)
+          end
+          G.E_MANAGER:add_event(Event({
+            func = function()
+              for i = 1, #G.GAME.tags do
+                if G.GAME.tags[i]:apply_to_run({ type = 'new_blind_choice' }) then
+                  break
                 end
-                if #jokers > 0 then
-                  local joker = jokers[#jokers]
-                  local edition = {polychrome = true}
-                  joker:set_edition(edition, true)
-                end
-                return true
-			end,
-            function() 
-                if #G.jokers.cards > 0 then
-                    for i = 1, #G.jokers.cards do
-                        if G.jokers.cards[i].edition == nil then
-                            return true
-                        end
-                    end
-                end
-                return false
-            end)
-            tag.triggered = true
+              end
+              return true
+            end
+          }))
+          return true
+        end,
+        function()
+          if #G.jokers.cards > 0 then
             return true
-        end
-    end,
+          end
+          return false
+        end)
+      if #G.jokers.cards > 0 then
+        tag.triggered = true
+      end
+      return true
+    end
+  end,
 }
-return {name = "Tags", items = {omnichrome_tag}}
+return { name = "Tags", items = { omnichrome_tag } }
