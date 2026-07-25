5f16ce1328c5cfad15435e66972218112d9dc53f
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-07-25 12:39:56 -0400

    Jeremy Bearimy and Pierrette Fix?
    Just reverted the code for them as i am not sure if the former code had a problem and i had trouble figuring out an alternative.

--- Items/Jokers/jeremy_bearimy.lua
+++ Items/Jokers/jeremy_bearimy.lua
@@ -26,21 +26,50 @@ local jeremy_bearimy = {
         }
     end,
 
+    --calculate = function(self, card, context)
+    --    if context.retrigger_joker_check and not context.retrigger_joker then
+    --        local other_context = context.other_context
+    --        if other_context.setting_blind then
+    --            return {
+    --                repetitions = 1,
+    --            }
+    --        end
+    --        if other_context.end_of_round and other_context.main_eval then
+    --            return {
+    --                repetitions = 1,
+    --            }
+    --        end
+    --    end
+    --end
+
     calculate = function(self, card, context)
-        if context.retrigger_joker_check and not context.retrigger_joker then
-            local other_context = context.other_context
-            if other_context.setting_blind then
-                return {
-                    repetitions = 1,
-                }
-            end
-            if other_context.end_of_round and other_context.main_eval then
-                return {
-                    repetitions = 1,
-                }
-            end
-        end
-    end
+      if context.setting_blind then
+          if not context.j_aij_jeremy_bearimy_triggered then
+              context.j_aij_jeremy_bearimy_triggered = true
+              local ret = SMODS.calculate_context(context)
+              if ret.calculated then
+                  return {
+                      message = localize('k_again_ex'),
+                      colour = G.C.FILTER,
+                      card = context.blueprint_card or card
+                  }
+              end
+          end
+      end
+      if context.end_of_round and context.main_eval then
+          if not context.j_aij_jeremy_bearimy_triggered then
+              context.j_aij_jeremy_bearimy_triggered = true
+              local ret = SMODS.calculate_context(context)
+              if ret.calculated then
+                  return {
+                      message = localize('k_again_ex'),
+                      colour = G.C.FILTER,
+                      card = context.blueprint_card or card
+                  }
+              end
+          end
+      end
+  end
 }
 
 return { name = { "Jokers" }, items = { jeremy_bearimy } }
--- Items/Jokers/pierrette.lua
+++ Items/Jokers/pierrette.lua
@@ -19,66 +19,65 @@ local pierrette = {
 
   end,
 
-  -- calculate = function(self, card, context)
-  --     if context.discard then
-  --         if not context.all_in_jest or (context.all_in_jest and not context.all_in_jest.re_discard) then
-  --             context.all_in_jest = context.all_in_jest or {}
-  --             context.all_in_jest.re_discard = true
-  --             local ret = SMODS.calculate_context(context)
-  --             if ret.calculated then
-  --                 return {
-  --                     message = localize('k_again_ex'),
-  --                     colour = G.C.FILTER,
-  --                     card = context.blueprint_card or card
-  --                 }
-  --             end
-  --         end
-  --     end
-  --     if context.pre_discard then
-  --         if not context.all_in_jest or (context.all_in_jest and not context.all_in_jest.re_pre_discard) then
-  --             context.all_in_jest = context.all_in_jest or {}
-  --             context.all_in_jest.re_pre_discard = true
-  --             local ret = SMODS.calculate_context(context)
-  --             if ret.calculated then
-  --                 return {
-  --                     message = localize('k_again_ex'),
-  --                     colour = G.C.FILTER,
-  --                     card = context.blueprint_card or card
-  --                 }
-  --             end
-  --         end
-  --     end
-  -- end
-
+  -- Don't know what was wrong with this, just reverting for now.
   calculate = function(self, card, context)
-    if context.discard and not context.retrigger_joker then
-      -- Doing seals manually because SMODS doesn't let us retrigger them on discard
-      -- Remember to update this when SMODS inevitably fixes this
-      local _, calculated = context.other_card:calculate_seal(context)
-      if calculated then
-        card_eval_status_text(context.blueprint_card or card, 'extra', nil, percent, nil, {message = localize('k_again_ex'), colour = G.C.FILTER})
-        percent = (percent or 0) + (percent_delta or 0.08)
-        -- return {
-        --   message = localize("k_again_ex"),
-        --   colour = G.C.FILTER,
-        --   card = context.blueprint_card or card
-        -- }
+      if context.discard then
+          if not context.j_aij_pierrette_triggered then
+              context.j_aij_pierrette_triggered = true
+              local ret = SMODS.calculate_context(context)
+              if ret.calculated then
+                  return {
+                      message = localize('k_again_ex'),
+                      colour = G.C.FILTER,
+                      card = context.blueprint_card or card
+                  }
+              end
+          end
       end
-    end
-    if context.retrigger_joker_check and not context.retrigger_joker then
-      local other_context = context.other_context
-      if other_context.discard then
-        return {
-          repetitions = 1,
-        }
+      if context.pre_discard then
+          if not context.j_aij_pierrette_triggered then
+              context.j_aij_pierrette_triggered = true
+              local ret = SMODS.calculate_context(context)
+              if ret.calculated then
+                  return {
+                      message = localize('k_again_ex'),
+                      colour = G.C.FILTER,
+                      card = context.blueprint_card or card
+                  }
+              end
+          end
       end
-      if other_context.pre_discard then
-        return {
-          repetitions = 1,
-        }
-      end
-    end
   end
+
+  --calculate = function(self, card, context)
+  --  if context.discard and not context.retrigger_joker then
+  --    -- Doing seals manually because SMODS doesn't let us retrigger them on discard
+  --    -- Remember to update this when SMODS inevitably fixes this
+  --    local _, calculated = context.other_card:calculate_seal(context)
+  --    if calculated then
+  --      card_eval_status_text(context.blueprint_card or card, 'extra', nil, percent, nil, {message = localize('k_again_ex'), colour = G.C.FILTER})
+  --      percent = (percent or 0) + (percent_delta or 0.08)
+  --      -- return {
+  --      --   message = localize("k_again_ex"),
+  --      --   colour = G.C.FILTER,
+  --      --   card = context.blueprint_card or card
+  --      -- }
+  --    end
+  --  end
+  --  if context.retrigger_joker_check and not context.retrigger_joker then
+  --    local other_context = context.other_context
+  --    if other_context.discard then
+  --      return {
+  --        repetitions = 1,
+  --      }
+  --    end
+  --    if other_context.pre_discard then
+  --      return {
+  --        repetitions = 1,
+  --      }
+  --    end
+  --  end
+  --end
 
 }
 return { name = { "Jokers" }, items = { pierrette } }
