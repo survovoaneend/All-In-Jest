1047c25477ea6ce1f085da0c8f89a1af3ad4aebb
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-07-26 10:52:55 -0400

    Multiple Phoney Baloney Fix

--- Items/Jokers/phoney_baloney.lua
+++ Items/Jokers/phoney_baloney.lua
@@ -28,7 +28,8 @@ local phoney_baloney = {
     end,
 
     calculate = function(self, card, context)
-        if context.reroll_shop and not context.blueprint then
+        if context.reroll_shop and not context.blueprint and not G.aij_phoney_baloney_triggered then
+            G.aij_phoney_baloney_triggered = true
             if card.ability.extra.rerolls_remaining <= 1 and context.cost == 0 then
                 SMODS.destroy_cards(card, nil, nil, true)
                 return {
@@ -44,6 +45,9 @@ local phoney_baloney = {
                     colour = G.C.FILTER
                 }
             end
+        end
+        if context.all_in_jest and context.all_in_jest.after_reroll_shop then
+            G.aij_phoney_baloney_triggered = nil
         end
         if context.end_of_round and context.main_eval and not context.blueprint then
             if context.beat_boss then
