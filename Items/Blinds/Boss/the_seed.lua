9283904a4ac27d8485dc658db13e44683e3df383
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-07-28 17:31:36 -0400

    The Seed

---
+++ Items/Blinds/Boss/the_seed.lua
@@ -0,0 +1,26 @@
+local the_seed = {
+    object_type = "Blind",
+    key = 'the_seed',
+    
+    boss = {
+        min = 1,
+    },
+    mult = 2,
+    boss_colour = HEX("d4ccbc"),
+    atlas = 'blinds',
+    pos = { X = 0, y = 85},
+    ignore = true,
+    order = 85,
+    dollars = 5,
+
+    defeat = function(self)
+        local temp = G.GAME.blind and G.GAME.blind.disabled
+        if temp then
+            return
+        end
+        G.GAME.aij_has_big_boss = true
+        G.GAME.aij_big_boss_reset_ante = true
+        G.GAME.round_resets.blind_choices.Big_Boss = get_new_boss()
+    end
+}
+return { name = {"Blinds"}, items = {the_seed} }
