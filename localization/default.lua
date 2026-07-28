a0f1e0a6895564579468ce8eddf89d60bd257a8a
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-07-28 12:13:32 -0400

    (Re)Text Fix
    Oops

--- localization/default.lua
+++ localization/default.lua
@@ -8525,7 +8525,7 @@ return {
                 }
             },
             c_aij_typhon = {
-                name = 'The Typhon',
+                name = 'Typhon',
                 text = {
                     "Enhances {C:attention}#1#{} selected",
                     "cards into",
