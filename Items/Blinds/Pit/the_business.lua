7932ff9169a80e11e39c5c550934e59a743e562f
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-07-29 19:13:56 -0400

    Some More Blinds
    The Business, The Clown, The Connection, The Multitude, and The Trio.

---
+++ Items/Blinds/Pit/the_business.lua
@@ -0,0 +1,53 @@
+local the_business = {
+    object_type = "Blind",
+    key = 'the_business',
+    boss = {
+      min = 4,
+      all_in_jest = {
+          pit = true
+      }
+    },
+    in_pool = function(self)
+        return All_in_Jest.pit_blinds_in_play()
+    end,
+    mult = 2,
+    boss_colour = HEX("3e4b4d"),
+    ignore = true,
+    atlas = 'blinds_pit',
+    pos = {y = 21},
+    order = 522,
+    dollars = 6,
+    config = {},
+
+    calculate = function(self, blind, context)
+        local temp = G.GAME.blind and G.GAME.blind.disabled
+        if temp then
+            return
+        end
+        local exclude_contexts = context.individual or context.repetition or context.blueprint
+        if context.end_of_round and not temp and not exclude_contexts then
+            local destroyable_jokers = {}
+            for i = 1, #G.jokers.cards do
+                if not SMODS.is_eternal(G.jokers.cards[i]) then
+                    table.insert(destroyable_jokers, G.jokers.cards[i])
+                end
+            end
+            if #destroyable_jokers > 0 then
+                local card_index = pseudorandom('jest_the_business', 1, 2)
+                if card_index == 2 then
+                    SMODS.destroy_cards(destroyable_jokers[#destroyable_jokers])
+                else
+                    SMODS.destroy_cards(destroyable_jokers[1])
+                end
+                local card = create_card('Joker', G.jokers, nil, 0, nil, nil, nil, 'jest_chaos_tag')
+                card:add_sticker('eternal', true)
+                card:add_to_deck()
+                G.jokers:emplace(card)
+                card:start_materialize()
+                blind.triggered = true
+                blind:wiggle()
+            end
+        end
+    end
+}
+return { name = {"Pit Blinds"}, items = {the_business} }
---
+++ Items/Blinds/Pit/the_clown.lua
@@ -0,0 +1,77 @@
+local the_clown = {
+    object_type = "Blind",
+    key = 'the_clown',
+    boss = {
+      min = 4,
+      all_in_jest = {
+          pit = true
+      }
+    },
+    in_pool = function(self)
+        return All_in_Jest.pit_blinds_in_play()
+    end,
+    mult = 2,
+    boss_colour = HEX("e8dede"),
+    ignore = true,
+    atlas = 'blinds_pit',
+    pos = {y = 19},
+    order = 520,
+    dollars = 6,
+    config = {},
+
+    calculate = function(self, blind, context)
+        local temp = G.GAME.blind and G.GAME.blind.disabled
+        if temp then
+            return
+        end
+        if context.first_hand_drawn and not temp then
+            local cards = {}
+            for k, v in pairs(G.hand.cards) do
+                cards[k] = k
+            end
+            local val = math.min(#G.hand.cards, 5)
+            while #G.hand.highlighted < val do
+                local index = pseudorandom_element(cards, pseudoseed('jest_the_clown'))
+                G.hand.cards[index].ability.aij_temp_always_scores = true
+                G.hand:add_to_highlighted(G.hand.cards[index])
+                cards[index] = nil
+            end
+            G.FUNCS.play_cards_from_highlighted(nil, true)
+            ease_hands_played(1)
+            G.GAME.aij_add_next_hand_to_blind = true
+        end
+        if context.after and not temp then
+            if G.GAME.aij_add_next_hand_to_blind then
+                G.GAME.aij_add_next_hand_to_blind = nil
+                G.E_MANAGER:add_event(Event({
+                  trigger = 'ease',
+                  blocking = false,
+                  ref_table = G.GAME,
+                  ref_value = 'chips',
+                  ease_to = 0,
+                  delay =  0.5,
+                  func = (function(t) return math.floor(t) end)
+                }))
+                All_in_Jest.ease_blind_requirement(nil, G.GAME.all_in_jest.advanced_hand_usage_blind[1].total_chips)
+                blind.triggered = true
+            end
+        end
+    end,
+
+    disable = function(self)
+        if G.GAME.all_in_jest.advanced_hand_usage_blind[1] then
+            All_in_Jest.ease_blind_requirement(nil, -G.GAME.all_in_jest.advanced_hand_usage_blind[1].total_chips)
+        end
+    end,
+
+    defeat = function(self)
+        local temp = G.GAME.blind and G.GAME.blind.disabled
+        if temp then
+            return
+        end
+        if G.GAME.all_in_jest.advanced_hand_usage_blind[1] then
+            All_in_Jest.ease_blind_requirement(nil, -G.GAME.all_in_jest.advanced_hand_usage_blind[1].total_chips)
+        end
+    end
+}
+return { name = {"Pit Blinds"}, items = {the_clown} }
