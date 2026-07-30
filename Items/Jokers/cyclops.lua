1986a306fcee240ee0332ebe13614919d27448e3
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-07-29 22:34:59 -0400

    Cyclops Fix

--- Items/Jokers/cyclops.lua
+++ Items/Jokers/cyclops.lua
@@ -1,50 +1,46 @@
 local cyclops = {
-  object_type = "Joker",
-  order = 177,
-  ignore = true,
-  key = "cyclops",
-  config = {
-    extra = {
-      repetitions = 4
-    }
-  },
-  attributes = {},
-  rarity = 3,
-  pos = { x = 20, y = 6 },
-  atlas = 'joker_atlas',
-  cost = 9,
-  unlocked = true,
-  discovered = false,
-  blueprint_compat = true,
-  eternal_compat = true,
-
-  loc_vars = function(self, info_queue, card)
-    return {
-      vars = {
-        card.ability.extra.repetitions
-      }
-    }
-  end,
-
-  calculate = function(self, card, context)
-    if context.joker_main and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
-      if #context.full_hand == 1 and context.full_hand[1]:get_id() == 14 then
-        G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
-        G.E_MANAGER:add_event(Event({
-          func = (function()
-            SMODS.add_card {
-              set = 'Tarot',    
-            }
-            G.GAME.consumeable_buffer = 0
-            return true
-          end)
-        }))
-        return {
-          message = localize('k_plus_tarot'),
-          colour = G.C.SECONDARY_SET.Tarot,
+    object_type = "Joker",
+    order = 177,
+    key = "cyclops",
+    config = {
+        extra = {
+            mult = 0,
+            mult_mod = 3,
         }
-      end
+    },
+    attributes = {},
+    rarity = 3,
+    pos = { x = 20, y = 6 },
+    ignore = true,
+    atlas = 'joker_atlas',
+    cost = 9,
+    unlocked = true,
+    discovered = false,
+    blueprint_compat = true,
+    eternal_compat = true,
+    
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.mult_mod,
+                card.ability.extra.mult
+            }
+        }
+    end,
+    
+    calculate = function(self, card, context)
+        if context.before and context.full_hand and #context.full_hand == 1 and G.GAME.current_round.hands_played == 0 then
+            SMODS.scale_card(card, {
+	            ref_table = card.ability.extra,
+                ref_value = "mult",
+	            scalar_value = "mult_mod",
+            })
+        end
+        if context.joker_main then
+            return {
+                mult = card.ability.extra.mult,
+            }
+        end
     end
-  end
 }
 return { name = { "Jokers" }, items = { cyclops } }
