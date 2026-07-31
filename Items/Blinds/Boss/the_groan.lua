a98722f5cf6e9573dd86d5c19baae2b26b61d2bf
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-07-31 20:26:02 +1000

    The Groan no longer triggers in a way to make the player feel cheated

--- Items/Blinds/Boss/the_groan.lua
+++ Items/Blinds/Boss/the_groan.lua
@@ -16,9 +16,11 @@ local the_groan = {
         if temp then
             return
         end
-        if context.all_in_jest and context.all_in_jest.drew_cards and not temp then
-            All_in_Jest.ease_blind_requirement(1, 0)
-            blind.triggered = true
+        if context.after then
+            if G.GAME.chips + SMODS.calculate_round_score() < G.GAME.blind.chips then
+                All_in_Jest.ease_blind_requirement(1, 0)
+                blind.triggered = true
+            end
         end
     end,
 
