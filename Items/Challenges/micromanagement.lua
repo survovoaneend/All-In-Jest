983ac57712cfa48c799a0b22c050652c3ff3da39
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-08-02 14:12:52 -0400

    Challenging

---
+++ Items/Challenges/micromanagement.lua
@@ -0,0 +1,23 @@
+local micromanagement = {
+    object_type = "Challenge",
+    key = 'micromanagement',
+    unlocked = function(self) 
+        if G and G.P_CENTERS and G.P_CENTERS['j_aij_office_assistant'].discovered == true then
+            return true 
+        end
+    end,
+    ignore = true,
+    jokers = {
+        { id = 'j_aij_office_assistant', eternal = true, edition = 'negative' },
+    },
+    rules = {
+        custom = {
+            {id = 'aij_all_jokers_perishable_1'},
+            {id = 'aij_all_jokers_perishable_2'},
+        },
+    },
+    apply = function(self) 
+        G.GAME.perishable_rounds = 2
+    end,
+}
+return { name = {"Challenges"}, items = {micromanagement} }
---
+++ Items/Challenges/pandemonium.lua
@@ -0,0 +1,17 @@
+local pandemonium = {
+    object_type = "Challenge",
+    key = 'pandemonium',
+    unlocked = function(self) 
+        if G and G.P_TAGS and G.P_TAGS['tag_aij_chaos'].discovered == true then
+            return true 
+        end
+    end,
+    ignore = true,
+    rules = {
+        custom = {
+            {id = 'aij_pandemonium_1'},
+            {id = 'aij_pandemonium_2'},
+        },
+    },
+}
+return { name = {"Challenges"}, items = {pandemonium} }
