1499e66c9da6a4cf91204aa0cf7594b746afb179
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-08-07 07:34:47 -0400

    Gonella and Antonio

--- Items/Jokers/antonio.lua
+++ Items/Jokers/antonio.lua
@@ -1,36 +1,46 @@
 local antonio = {
     object_type = "Joker",
     order = 10004,
-    ignore = true,
 
     key = "antonio",
     config = {
         extra = {
-        
+            mult_mod = 1
         }
     },
-    attributes = {},
+    attributes = { 'modify_card', 'perma_bonus', 'mult' },
     rarity = 4,
-    -- unlock_condition = { hidden = true },
+    unlock_condition = { hidden = true },
     pos = { x = 7, y = 14},
-    atlas = legendary_atlas,
+    atlas = 'legendary_atlas',
     cost = 20,
-    unlocked = true,
+    unlocked = false,
     discovered = false,
     blueprint_compat = true,
     eternal_compat = true,
-    -- soul_pos = { x = 7, y = 15},
+    soul_pos = { x = 7, y = 15},
 
     loc_vars = function(self, info_queue, card)
         return { 
             vars = {
-            
+                card.ability.extra.mult_mod
             }
         }
     end,
 
     calculate = function(self, card, context)
-
+        if context.individual and context.cardarea == G.play then
+            for k, v in pairs(G.playing_cards) do
+                if v:get_id() == context.other_card:get_id() then
+                    v.ability.perma_mult = v.ability.perma_mult or 0
+                    v.ability.perma_mult = v.ability.perma_mult + card.ability.extra.mult_mod
+                end
+            end
+            return {
+                extra = { message = localize('k_upgrade_ex'), colour = G.C.MULT },
+                card = card
+            }
+        end
     end
 }
 return { name = {"Jokers"}, items = {antonio} }
