6cec6d2bc7e43457783f85ee0d93ee6238b84e2c
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-08-07 14:25:42 -0400

    Soft Serve Rework

--- Items/Jokers/soft_serve.lua
+++ Items/Jokers/soft_serve.lua
@@ -4,8 +4,8 @@ local soft_serve = {
     key = "soft_serve",
     config = {
         extra = {
-            total_round = 3,
-            rounds_left = 3
+            total_purchases = 4,
+            purchases_left = 4
         }
     },
     attributes = { 'passive', 'food', 'joker', 'tarot', 'planet', 'shop_slot', },
@@ -21,24 +21,21 @@ local soft_serve = {
     loc_vars = function(self, info_queue, card)
         return {
             vars = {
-                card.ability.extra.rounds_left,
-                card.ability.extra.total_round
+                card.ability.extra.purchases_left,
+                card.ability.extra.total_purchases
             }
         }
     end,
 
     calculate = function(self, card, context)
-        if context.starting_shop and not context.blueprint then
-            G.GAME.aij_soft_serve_trigger = true
-        end
         if context.modify_final_cashout and not context.blueprint then
             G.GAME.aij_exculde_vanilla_in_shop = G.GAME.aij_exculde_vanilla_in_shop or {['Joker'] = true, ['Tarot'] = true, ['Tarot_Planet'] = true, ['Planet'] = true}
         end
-        if context.ending_shop and G.GAME.aij_soft_serve_trigger and not context.blueprint then
+        if not context.buying_self and context.buying_card and context.card.config.center.set and (G.GAME.aij_exculde_vanilla_in_shop[context.card.config.center.set] or context.card.config.center.set == 'Booster') and not context.blueprint then
             G.GAME.aij_soft_serve_trigger = nil
             local remaining_mod = {}
             remaining_mod.num = 1
-            if (card.ability.extra.rounds_left - 1) <= 0 then
+            if (card.ability.extra.purchases_left - 1) <= 0 then
                 SMODS.destroy_cards(card, nil, nil, true)
                 return {
                     message = localize('k_eaten_ex'),
@@ -47,12 +44,12 @@ local soft_serve = {
             else
                 SMODS.scale_card(card, {
                     ref_table = card.ability.extra,
-                    ref_value = "rounds_left",
+                    ref_value = "purchases_left",
                     scalar_table = remaining_mod,
                     scalar_value = "num",
                     operation = '-',
                     scaling_message = {
-                        message = localize{key='a_remaining', type='variable', vars={card.ability.extra.rounds_left-1}},
+                        message = localize{key='a_remaining', type='variable', vars={card.ability.extra.purchases_left-1}},
                         colour = G.C.FILTER
                     },
                 })
