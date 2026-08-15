c2acdb3da09a80c6591d96845ebaf22500498b1e
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2026-08-15 16:12:32 -0400

    Mint Condition Text

--- localization/default.lua
+++ localization/default.lua
@@ -319,6 +319,7 @@ return {
             pinned = "Pinned",
             aij_unusual_doubled = "Doubled",
             aij_recherche_doubled = "Doubled",
+            aij_mint_condition = "Mint Condition",
             -- Seals
             aij_smiley_seal = "Smiley Seal",
             aij_melted_seal = "Melted Seal",
@@ -10863,6 +10864,13 @@ return {
                 text = {
                     '{C:attention}X2{} effects',
                     '{C:inactive,s:0.8}(Granted by a Recherche Tag){}'
+                }
+            },
+            aij_mint_condition = {
+                name = "Mint Condition",
+                text = {
+                    'This {C:attention}Joker{} costs an',
+                    'additional {C:money}$10'
                 }
             },
             -- (not a sticker but treated as such)
