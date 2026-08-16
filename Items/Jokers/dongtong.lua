09555be77c14e3debc9dbdf0072bd4ec6350e262
vitellaryjr <43586575+vitellaryjr@users.noreply.github.com>
2026-08-15 21:45:21 -0700

    fix crash on dev smods when starting new run
    no clue what is possibly crashing tbh. but this order of logic seems a bit better to me anyway so this'll probably prevent some other obscure crash in the future as well

--- Items/Jokers/dongtong.lua
+++ Items/Jokers/dongtong.lua
@@ -114,7 +114,7 @@ local dongtong = {
 -- (E.g., without this Juggler would only give +1 hand size even with Dongtong owned)
 local aij_card_add_to_deck = Card.add_to_deck
 function Card:add_to_deck(from_debuff)
-    if not self.added_to_deck and G.jokers and (self.config.center.dongtong_compat == nil or self.config.center.dongtong_compat) and contains_number(self.config.center.config, { x_chips = 1, x_mult = 1, extra_value = true, rarity = true, card_limit = true }) and (self.area == nil or contains(SMODS.get_card_areas('jokers'), self.area)) then
+    if not self.added_to_deck and G.jokers and (self.area == nil or contains(SMODS.get_card_areas('jokers'), self.area)) and (self.config.center.dongtong_compat ~= false) and contains_number(self.config.center.config, { x_chips = 1, x_mult = 1, extra_value = true, rarity = true, card_limit = true }) then
         dong_tong_double(self)
     end
     return aij_card_add_to_deck(self, from_debuff)
@@ -124,7 +124,7 @@ end
 local updateref = Card.update
 function Card:update(dt)
     local ref = updateref(self, dt)
-    if G.jokers and self.ability.set == 'Joker' and (self.config.center.dongtong_compat == nil or self.config.center.dongtong_compat) and contains_number(self.config.center.config, { x_chips = 1, x_mult = 1, extra_value = true, rarity = true, card_limit = true }) and contains(SMODS.get_card_areas('jokers'), self.area) then
+    if G.jokers and self.ability.set == 'Joker' and contains(SMODS.get_card_areas('jokers'), self.area) and (self.config.center.dongtong_compat ~= false) and contains_number(self.config.center.config, { x_chips = 1, x_mult = 1, extra_value = true, rarity = true, card_limit = true }) then
         dong_tong_double(self)
     end
     return ref
