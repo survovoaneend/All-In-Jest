54edb2bbe317f3ac7129404c01f7d91d0f29bca2
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-09-03 21:20:44 +1000

    Urns works with the artists. Urns and Sacrifice now compatible with eternal playing cards

--- Items/Consumables/Tarots/sacrifice.lua
+++ Items/Consumables/Tarots/sacrifice.lua
@@ -1,5 +1,5 @@
 local sacrifice = {
-    object_type = "Consumable",
+  object_type = "Consumable",
 	key = 'sacrifice',
 	set = 'Tarot',
 	pos = { x = 14, y = 2 },
@@ -8,7 +8,7 @@ local sacrifice = {
 	discovered = false,
 	order = 11,
 	config = { max_highlighted = 1 },
-    attributes = {'generation', 'destroy_card', 'playing_card', 'enhancements'},
+  attributes = {'generation', 'destroy_card', 'playing_card', 'enhancements'},
 	atlas = 'consumable_atlas',
 	loc_vars = function(self, info_queue, card)
 		return {
@@ -18,26 +18,35 @@ local sacrifice = {
 		}
 	end,
 	can_use = function(self, card)
-        if G.hand and (#G.hand.highlighted <= card.ability.max_highlighted and #G.hand.highlighted > 0) then
-            return true
+    if G.hand and (#G.hand.highlighted <= card.ability.max_highlighted and #G.hand.highlighted > 0) then
+      for _, v in pairs(G.hand.highlighted) do
+        if SMODS.is_eternal(v, {destroy_cards = true}) then
+          return false
         end
-    end,
+      end
+      return true
+    end
+  end,
 	use = function(self, card)
-        SMODS.destroy_cards(G.hand.highlighted, nil, true)
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                local front = pseudorandom_element(G.P_CARDS, pseudoseed('sacrifice'))
-                local enhance = SMODS.poll_enhancement({guaranteed = true, no_replace = true, key = 'sacrifice'})
-                local _card = create_playing_card({
-                    front = front,
-                    center = G.P_CENTERS[enhance]}, G.hand, nil, nil, {G.C.SECONDARY_SET.Enhanced})
-                G.GAME.blind:debuff_card(_card)
-                G.hand:sort()
-                playing_card_joker_effects({_card})
-                _card:juice_up(0.3, 0.5)
-                return true
-            end
-        }))
-    end,
+    SMODS.destroy_cards(G.hand.highlighted, nil, true)
+    G.E_MANAGER:add_event(Event({
+      func = function()
+        local front = pseudorandom_element(G.P_CARDS, pseudoseed('sacrifice'))
+        local enhance = SMODS.poll_enhancement({guaranteed = true, no_replace = true, key = 'sacrifice'})
+        local _card = create_playing_card(
+          {front = front, center = G.P_CENTERS[enhance]}, 
+          G.hand, 
+          nil,
+          nil, 
+          {G.C.SECONDARY_SET.Enhanced}
+        )
+        G.GAME.blind:debuff_card(_card)
+        G.hand:sort()
+        playing_card_joker_effects({_card})
+        _card:juice_up(0.3, 0.5)
+        return true
+      end
+    }))
+  end,
 }
 return {name = {"Tarots"}, items = {sacrifice}}
--- Items/Consumables/Tarots/urns.lua
+++ Items/Consumables/Tarots/urns.lua
@@ -21,91 +21,85 @@ local urns = {
 	can_use = function(self, card)
     if G.hand and #G.hand.cards >= card.ability.ran_cards and (#G.hand.highlighted <= card.ability.max_highlighted and #G.hand.highlighted > 0) then
 			for _, v in pairs(G.hand.highlighted) do
-				if v.config.center ~= G.P_CENTERS.c_base then
-					return true
+				if v.config.center == G.P_CENTERS.c_base or SMODS.is_eternal(v, {destroy_cards = true}) then
+					return false
 				end
 			end
+      return true
     end
   end,
 	use = function(self, card)
-    local center_key = G.hand.highlighted[1].config.center.key
-		for k, v in pairs(G.hand.highlighted) do
-			if v.config.center ~= G.P_CENTERS.c_base then
-				center_key = v.config.center.key
-				break
-			end
-		end
-		local other_center_key = G.hand.highlighted[1].config.aij_other_center['center'].key
-		for k, v in pairs(G.hand.highlighted) do
-			if v.config.aij_other_center then
-				other_center_key = v.config.aij_other_center['center'].key
-				break
-			end
-		end
-
-    SMODS.destroy_cards(G.hand.highlighted, nil, true)
-
-		local valid_cards = {}
-		for k, v in pairs(G.hand.cards) do
-        if not v.highlighted and not SMODS.has_enhancement(v, center_key)  then
-            valid_cards[#valid_cards+1] = v
+    for i = 1, #G.hand.highlighted do
+      local leftmost = G.hand.highlighted[1]
+      for i=1, #G.hand.highlighted do
+        if G.hand.highlighted[i].T.x < leftmost.T.x and not G.hand.highlighted[i].getting_sliced then
+          leftmost = G.hand.highlighted[i]
         end
-		end
-		local selected_cards = {}
-		for i = 1, card.ability.ran_cards do
-			local cur_card = pseudorandom_element(valid_cards, pseudoseed('urns'))
-			for k, v in pairs(valid_cards) do
-				if v == cur_card then
-					valid_cards[k] = nil
-				end
-			end
-			selected_cards[#selected_cards+1] = cur_card
-		end
+      end
 
-		G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
-      play_sound('tarot1')
-      card:juice_up(0.3, 0.5)
-      return true
-    end }))
+      local center_key = leftmost.config.center.key
+      local other_center_key = nil
+      if leftmost.config.aij_other_center then
+          other_center_key = v.config.aij_other_center['center'].key
+      end
 
-    for i=1, #selected_cards do
-      local percent = 1.15 - (i-0.999)/(#selected_cards-0.998)*0.3
-      G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function()
-        selected_cards[i]:flip()
-        play_sound('card1', percent)
-        selected_cards[i]:juice_up(0.3, 0.3)
-        return true
-      end }))
-    end
-    
-    G.E_MANAGER:add_event(Event({
-      func = function()
-        for i = 1, #selected_cards do
-          selected_cards[i]:set_ability(G.P_CENTERS[center_key])
-          if other_center_key then
-            All_in_Jest.set_other_enhancement(selected_cards[i], other_center_key)
+      SMODS.destroy_cards(leftmost)
+
+      local valid_cards = {}
+      for _, v in pairs(G.hand.cards) do
+        if not v.highlighted and not SMODS.has_enhancement(v, center_key)  then
+          valid_cards[#valid_cards+1] = v
+        end
+      end
+      local selected_cards = {}
+      for _ = 1, card.ability.ran_cards do
+        local cur_card = pseudorandom_element(valid_cards, pseudoseed('urns'))
+        for k, v in pairs(valid_cards) do
+          if v == cur_card then
+            valid_cards[k] = nil
           end
         end
-        return true
+        selected_cards[#selected_cards+1] = cur_card
       end
-    }))
 
-		for i=1, #selected_cards do
-      local percent = 0.85 + (i-0.999)/(#selected_cards-0.998)*0.3
-      G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() 
-        selected_cards[i]:flip()
-        play_sound('tarot2', percent, 0.6)
-        selected_cards[i]:juice_up(0.3, 0.3)
+      G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+        play_sound('tarot1')
+        card:juice_up(0.3, 0.5)
         return true
       end }))
+
+      for j=1, #selected_cards do
+        local percent = 1.15 - (j-0.999)/(#selected_cards-0.998)*0.3
+        G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function()
+          selected_cards[j]:flip()
+          play_sound('card1', percent)
+          selected_cards[j]:juice_up(0.3, 0.3)
+          return true
+        end }))
+      end
+      
+      G.E_MANAGER:add_event(Event({
+        func = function()
+          for j = 1, #selected_cards do
+            selected_cards[j]:set_ability(G.P_CENTERS[center_key])
+            if other_center_key then
+              All_in_Jest.set_other_enhancement(selected_cards[j], other_center_key)
+            end
+          end
+          return true
+        end
+      }))
+
+      for j=1, #selected_cards do
+        local percent = 0.85 + (j-0.999)/(#selected_cards-0.998)*0.3
+        G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() 
+          selected_cards[j]:flip()
+          play_sound('tarot2', percent, 0.6)
+          selected_cards[j]:juice_up(0.3, 0.3)
+          return true
+        end }))
+      end
     end
-
-    G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2,func = function() 
-      G.hand:unhighlight_all()
-      return true
-    end }))
-
-    delay(0.5)
   end,
 }
 return {name = {"Tarots"}, items = {urns}}
