8ba88dd0068686d9ab7efad14f06d5e2267f7490
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-09-03 21:40:23 +1000

    Moved a hook to context.lua

--- Utils/context.lua
+++ Utils/context.lua
@@ -1,3 +1,5 @@
+
+-- Add cashing_out context
 local cash_out_ref = G.FUNCS.cash_out
 G.FUNCS.cash_out = function(e)
   SMODS.calculate_context({
@@ -6,7 +8,10 @@ G.FUNCS.cash_out = function(e)
 
   cash_out_ref(e)
 end
---Stuff for destruction effects
+
+-- Stuff for destruction effects
+-- Adds jest_destroying_or_selling_joker, jest_destroying_joker, jest_destroying_or_selling_card, jest_destroying_card
+-- (Is this necessary anymore?)
 local start_dissolve_ref = Card.start_dissolve
 function Card:start_dissolve(dissolve_colours, silent, dissolve_time_fac, no_juice)
   if G.jokers and self.ability.set == 'Joker' then
@@ -62,6 +67,23 @@ function Card:shatter()
     if self.ability.aij_pc_eternal then return end
   end
   return shatter_ref(self)
+end
+
+-- For Bizco, taken from paperback
+local calculate_main_scoring_ref = SMODS.calculate_main_scoring
+function SMODS.calculate_main_scoring(context, scoring_hand)
+  calculate_main_scoring_ref(context, scoring_hand)
+  if context.cardarea == G.play or context.cardarea == 'unscored' then
+    SMODS.calculate_context {
+      all_in_jest = {
+        after_scoring_cards = true 
+      },
+      full_hand = G.play.cards,
+      scoring_hand = context.scoring_hand,
+      scoring_name = context.scoring_name,
+      poker_hands = context.poker_hands
+    }
+  end
 end
 
 -- Workshoping
--- Utils/hooks.lua
+++ Utils/hooks.lua
@@ -427,22 +427,6 @@ function Card:set_sell_value()
     end
 end
 
--- For Bizco, taken from paperback
-local calculate_main_scoring_ref = SMODS.calculate_main_scoring
-function SMODS.calculate_main_scoring(context, scoring_hand)
-  calculate_main_scoring_ref(context, scoring_hand)
-  if context.cardarea == G.play or context.cardarea == 'unscored' then
-    SMODS.calculate_context {
-      all_in_jest = {
-        after_scoring_cards = true 
-      },
-      full_hand = G.play.cards,
-      scoring_hand = context.scoring_hand,
-      scoring_name = context.scoring_name,
-      poker_hands = context.poker_hands
-    }
-  end
-end
 local get_front_spriteinfo_ref = get_front_spriteinfo
 function get_front_spriteinfo(_front)
     if _front.card and _front.card.ability and _front.card.ability.numbertaker_rankless and _front.suit then
