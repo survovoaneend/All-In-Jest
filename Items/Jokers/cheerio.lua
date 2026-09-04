141bddc0afe622d42253cf442f65062e9cafc0fa
english5040 <ejwu@andrew.cmu.edu>
2026-09-03 21:52:13 -0400

    Cheerio: should not be eternal

--- Items/Jokers/cheerio.lua
+++ Items/Jokers/cheerio.lua
@@ -15,7 +15,7 @@ local cheerio = {
     unlocked = true,
     discovered = false,
     blueprint_compat = true,
-    eternal_compat = true,
+    eternal_compat = false,
     perishable_compat = true,
 
     loc_vars = function(self, info_queue, card)
