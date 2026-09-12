463be9b535a2f08629924823446cfecf9fa16ed0
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-09-12 18:07:18 +1000

    Split lovely.toml into seperate files

--- Items/Blinds/Boss/the_real.lua
+++ Items/Blinds/Boss/the_real.lua
@@ -18,4 +18,18 @@ local the_real = {
         
     end
 }
+
+local aij_card_is_suit_ref = Card.is_suit
+function Card:is_suit(suit, bypass_debuff, flush_calc)
+    if G.GAME.blind and G.GAME.blind.config.blind.key == 'bl_aij_the_real' and not G.GAME.blind.disabled then
+        if SMODS.has_no_suit(self) then
+            return false
+        else
+            return self.base.suit == suit
+        end
+    else
+        return aij_card_is_suit_ref(self, suit, bypass_debuff, flush_calc)
+    end
+end
+
 return { name = {"Blinds"}, items = {the_real} }
