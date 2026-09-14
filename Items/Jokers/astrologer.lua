87d7064938344b8bb1bc0f1c7f9fe64e8c5608db
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-09-15 09:42:02 +1000

    Astrologer fix and code logic rework

--- Items/Jokers/astrologer.lua
+++ Items/Jokers/astrologer.lua
@@ -38,4 +38,36 @@ local astrologer = {
 		end
 	end,
 }
+
+-- Theoretically less RNG than changing the object pool, and does exactly what it says
+-- Also allows other planets without moons to appear, which seems fairer
+local ref_smods_poll_object_aij = SMODS.poll_object
+function SMODS.poll_object(args)
+	local output_key = ref_smods_poll_object_aij(args)
+
+	if
+		args.type == "Planet"
+		and G.P_CENTERS[output_key].set == "Planet"
+		and next(SMODS.find_card("j_aij_astrologer"))
+	then
+		for k, v in pairs(G.P_CENTER_POOLS.Planet) do
+			if v.config.moon and v.config.hand_type == G.P_CENTERS[output_key].config.hand_type then
+				if (G.GAME.round_resets.ante % 2) == 0 then
+					if v.config.chips then
+						output_key = v.key
+						break
+					end
+				else
+					if v.config.mult then
+						output_key = v.key
+						break
+					end
+				end
+			end
+		end
+	end
+
+	return output_key
+end
+
 return { name = { "Jokers" }, items = { astrologer } }
