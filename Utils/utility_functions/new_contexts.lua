c6ea2704e7f97f5563e61a573f492dd368df49a2
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-09-19 19:51:44 +1000

    Added comment for previous commit

--- Utils/utility_functions/new_contexts.lua
+++ Utils/utility_functions/new_contexts.lua
@@ -1,3 +1,7 @@
+
+-- Blacklist new contexts from being retriggered
+SMODS.CONTEXT_RETRIGGER_BLACKLIST["aij_retriggered_card"] = true
+
 -- Add cashing_out context
 local cash_out_ref = G.FUNCS.cash_out
 G.FUNCS.cash_out = function(e)
@@ -82,8 +86,6 @@ function SMODS.calculate_main_scoring(context, scoring_hand)
 		})
 	end
 end
-
-SMODS.CONTEXT_RETRIGGER_BLACKLIST["aij_retriggered_card"] = true
 
 -- Workshoping
 -- context.chance_trigger.trigger (returns true when 1 in whatever chance is true and false when it fails)
