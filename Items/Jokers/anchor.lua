ee1f6bf45831dcdfa6f33aa8b363a892b45699ce
jumbocarrot0 <jumbocarrot0@gmail.com>
2026-09-21 17:56:20 +1000

    Tidied up Anchor logic

--- Items/Jokers/anchor.lua
+++ Items/Jokers/anchor.lua
@@ -18,42 +18,34 @@ local anchor = {
 	loc_vars = function(self, info_queue, card) end,
 
 	calculate = function(self, card, context)
-		-- I probably don't need to use both joker_type_destroyed and check_eternal
-		-- Limited testing suggested joker_type_destroyed didn't even work in SMODS~BETA-0827c
-		-- But I'm using both anyways
 		if
-			context.joker_type_destroyed
-			or (context.check_eternal and not context.trigger.from_sell) and not context.blueprint
+			context.joker_type_destroyed and not context.blueprint
 		then
-			local card_to_be_destroyed = context.card or context.other_card
-			if card_to_be_destroyed.ability.jest_sold_self then
-				-- Do not trigger on sold cards
-			else
-				local source_card = context.blueprint_card or card
+			local card_to_be_destroyed = context.card
+			local source_card = context.blueprint_card or card
 
-				-- Do not destroy anchors
-				if card_to_be_destroyed == source_card then
+			-- Do not destroy anchors
+			if card_to_be_destroyed == source_card then
+				return {
+					no_destroy = { override_compat = true },
+				}
+			else
+				-- Check if anchor is to the left or right
+				local index_of_destroyed_card = 0
+				if G.jokers and G.jokers.cards then
+					for i = 1, #G.jokers.cards do
+						if G.jokers.cards[i] == card_to_be_destroyed then
+							index_of_destroyed_card = i
+						end
+					end
+				end
+				local left = index_of_destroyed_card - 1
+				local right = index_of_destroyed_card + 1
+				local is_nearby = (G.jokers.cards[left] == source_card) or (G.jokers.cards[right] == source_card)
+				if is_nearby then
 					return {
 						no_destroy = { override_compat = true },
 					}
-				else
-					-- Check if anchor is to the left or right
-					local index_of_destroyed_card = 0
-					if G.jokers and G.jokers.cards then
-						for i = 1, #G.jokers.cards do
-							if G.jokers.cards[i] == card_to_be_destroyed then
-								index_of_destroyed_card = i
-							end
-						end
-					end
-					local left = index_of_destroyed_card - 1
-					local right = index_of_destroyed_card + 1
-					local is_nearby = (G.jokers.cards[left] == source_card) or (G.jokers.cards[right] == source_card)
-					if is_nearby then
-						return {
-							no_destroy = { override_compat = true },
-						}
-					end
 				end
 			end
 		end
